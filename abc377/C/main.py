3b7f134781b52bd1af1eaac8540aad103ea356a4
masahiro-999 <masahiro999@outlook.com>
2004-10-26 23:00:00 +0900

    abc377_abc

---
+++ abc377/C/main.py
@@ -0,0 +1,43 @@
+import sys
+import os
+from math import ceil, floor, sqrt, pi, factorial, gcd,lcm,sin,cos,tan,asin,acos,atan2,exp,log,log10, isqrt
+from collections import Counter, defaultdict, deque
+from copy import deepcopy
+from functools import cmp_to_key, lru_cache, reduce, cache
+from operator import add, iand, ior, itemgetter, mul, xor
+from string import ascii_lowercase, ascii_uppercase, ascii_letters
+from typing import *
+from bisect import bisect, bisect_left, bisect_right
+from heapq import heapify, heappop, heappush, heappushpop, nlargest, nsmallest
+from sortedcontainers import SortedSet, SortedList, SortedDict
+from itertools import product, accumulate,permutations,combinations, count
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+I = input
+II = lambda: int(I())
+LI = lambda: list(input().split())
+LII = lambda: list(map(int, input().split()))
+sys.setrecursionlimit(10000000)
+inf = 100100100100
+debug = False
+# debug = True
+if debug:
+    def dprint(*arg): print(*arg, file=sys.stderr)
+else:
+    def dprint(*arg): pass
+
+N,M = LII()
+
+dxdy = ((2,1),(1,2),(-1,2),(-2,1),(-2,-1),(-1,-2),(1,-2),(2,-1))
+ng_pos = set()
+for _ in range(M):
+    a,b = LII()
+    ng_pos.add((a,b))
+    for dx, dy in dxdy:
+        x = a + dx
+        y = b + dy
+        if 1 <= x <= N and 1 <= y <= N:
+            ng_pos.add((x,y))
+
+ans = N*N - len(ng_pos)
+
+print(ans)
