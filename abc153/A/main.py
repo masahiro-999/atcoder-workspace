25cdbe6cfc7b255e242aec9299ba53c6b9baee09
masahiro-999 <masahiro999@outlook.com>
2023-04-18 18:05:42 +0900

    abc153_a

---
+++ abc153/A/main.py
@@ -0,0 +1,22 @@
+import sys
+from collections import deque, Counter
+sys.setrecursionlimit(5 * 10 ** 5)
+# from pypyjit import set_param
+# set_param('max_unroll_recursion=-1')
+input = lambda: sys.stdin.readline().rstrip()
+ii = lambda: int(input())
+mi = lambda: map(int, input().split())
+li = lambda: list(mi())
+inf = 2 ** 63 - 1
+
+
+
+def solve(H: int, A: int):
+    print( -(-H//A))
+
+def main():
+    H, A = mi()  # type: int
+    solve(H, A)
+    return
+
+main()
