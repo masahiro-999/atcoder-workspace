03b9c0bad23687ca302dd843036688e45b284d17
masahiro-999 <masahiro999@outlook.com>
2023-05-06 17:51:54 +0900

    abc071_d

---
+++ abc071/D/main.py
@@ -0,0 +1,115 @@
+import sys
+from collections import deque, Counter, defaultdict
+from itertools import product, accumulate
+sys.setrecursionlimit(5 * 10 ** 5)
+# from pypyjit import set_param
+# set_param('max_unroll_recursion=-1')
+input = lambda: sys.stdin.readline().rstrip()
+ii = lambda: int(input())
+mi = lambda: map(int, input().split())
+li = lambda: list(mi())
+inf = 2 ** 63 - 1
+
+
+MOD = 1000000007
+
+class ModInt:
+    def __init__(self, x):
+        self.x = x % MOD
+
+    def __str__(self):
+        return str(self.x)
+
+    __repr__ = __str__
+
+    def __add__(self, other):
+        return (
+            ModInt(self.x + other.x) if isinstance(other, ModInt) else
+            ModInt(self.x + other)
+        )
+
+    def __sub__(self, other):
+        return (
+            ModInt(self.x - other.x) if isinstance(other, ModInt) else
+            ModInt(self.x - other)
+        )
+
+    def __mul__(self, other):
+        return (
+            ModInt(self.x * other.x) if isinstance(other, ModInt) else
+            ModInt(self.x * other)
+        )
+
+    def __truediv__(self, other):
+        return (
+            ModInt(
+                self.x * pow(other.x, MOD - 2, MOD)
+            ) if isinstance(other, ModInt) else
+            ModInt(self.x * pow(other, MOD - 2, MOD))
+        )
+
+    def __pow__(self, other):
+        return (
+            ModInt(pow(self.x, other.x, MOD)) if isinstance(other, ModInt) else
+            ModInt(pow(self.x, other, MOD))
+        )
+
+    __radd__ = __add__
+
+    def __rsub__(self, other):
+        return (
+            ModInt(other.x - self.x) if isinstance(other, ModInt) else
+            ModInt(other - self.x)
+        )
+
+    __rmul__ = __mul__
+
+    def __rtruediv__(self, other):
+        return (
+            ModInt(
+                other.x * pow(self.x, MOD - 2, MOD)
+            ) if isinstance(other, ModInt) else
+            ModInt(other * pow(self.x, MOD - 2, MOD))
+        )
+
+    def __rpow__(self, other):
+        return (
+            ModInt(pow(other.x, self.x, MOD)) if isinstance(other, ModInt) else
+            ModInt(pow(other, self.x, MOD))
+        )
+
+def solve(N: int, S: "List[str]"):
+    s0 = S[0]
+    s1 = S[1]
+
+    i = 0
+    prev =-1 # 1 は縦ブロック 0は横ブロック
+    ans = ModInt(1)
+    while i<N:
+        if s0[i] == s1[i]:
+            if prev == -1:
+                ans *= 3
+            elif prev == 0:
+                pass
+            elif prev == 1:
+                ans *= 2
+            prev = 1
+        else:
+            i+=1
+            if prev == -1:
+                ans *= 6
+            elif prev == 0:
+                ans *= 3
+            elif prev == 1:
+                ans *= 2
+            prev = 0
+        i += 1
+    print(ans)
+
+def main():
+    N = ii()  # type: int
+    S = [input() for _ in range(2)]  # type: "List[str]"
+    solve(N, S)
+    return
+
+main()
