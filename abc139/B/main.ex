c125275cb16edb0c311369688c6eff2688c1a919
masahiro-999 <masahiro999@outlook.com>
2023-05-12 21:46:04 +0900

    abc139_b

--- abc139/B/main.ex
+++ abc139/B/main.ex
@@ -1,15 +1,22 @@
 defmodule Main do
 
+    def get_ans(num_tap, socket, a, b) do
+        cond do
+         socket >= b -> num_tap
+         num_tap == 0 -> get_ans(1, a, a, b)
+         :true -> get_ans(num_tap+1, socket+a-1, a, b)
+        end
+    end
     def solve(a,b) do
-        ans = Float.ceil(a/b)
+        ans = get_ans(0, 1, a, b)
         IO.puts(ans)
     end
 
     def main() do
-        a,b =
-            IO.read(:line)
+        [a,b] = IO.read(:line)
+            |> String.trim()
             |> String.split(" ")
-            |> Enum.map(String.to_integer)
+            |> Enum.map(&String.to_integer/1)
         solve(a,b)
     end
 end
