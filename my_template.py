0c465d6986978cab6aff32ee4eb852675aa448e6
masahiro-999 <masahiro999@outlook.com>
2023-06-04 11:25:03 +0900

    update template

--- my_template.py
+++ my_template.py
@@ -1,6 +1,10 @@
-import sys
+import sys, re
+from math import ceil, floor, sqrt, pi, factorial, gcd,sin,cos,tan,asin,acos,atan2,exp,log,log10
 from collections import deque, Counter, defaultdict
 from itertools import product, accumulate
+from functools import reduce,lru_cache
+from bisect import bisect
+from heapq import heapify, heappop, heappush
 sys.setrecursionlimit(5 * 10 ** 5)
 # from pypyjit import set_param
 # set_param('max_unroll_recursion=-1')
