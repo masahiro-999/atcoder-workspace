0e6ee4c0526d2a42250ba19a8f21e3ae8fab0e07
masahiro-999 <masahiro999@outlook.com>
2023-06-10 15:51:57 +0900

    abc096_c

---
+++ abc096/C/main.py
@@ -0,0 +1,46 @@
+import sys, re
+from math import ceil, floor, sqrt, pi, factorial, gcd,sin,cos,tan,asin,acos,atan2,exp,log,log10
+from collections import deque, Counter, defaultdict
+from itertools import product, accumulate
+from functools import reduce,lru_cache
+from bisect import bisect
+from heapq import heapify, heappop, heappush
+sys.setrecursionlimit(5 * 10 ** 5)
+# from pypyjit import set_param
+# set_param('max_unroll_recursion=-1')
+input = lambda: sys.stdin.readline().rstrip()
+ii = lambda: int(input())
+mi = lambda: map(int, input().split())
+li = lambda: list(mi())
+inf = 2 ** 63 - 1
+tokens = (i for line in iter(input, "") for i in line.split())
+
+YES = "Yes"
+NO = "No"
+
+def solve(H: int, W: int, s: "List[str]"):
+    def can_paint(i,j):
+        if s[i][j] == ".":
+            return True
+        else:
+            return is_black(i+1,j) or is_black(i-1,j) or is_black(i,j-1) or is_black(i,j+1)
+    def is_black(i,j):
+        try:
+            return s[i][j] == "#"
+        except:
+            return False
+    ans = YES
+    for i in range(H):
+        for j in range(W):
+            if not can_paint(i,j):
+                ans = NO
+    print(ans)
+
+def main():
+    H = int(next(tokens))  # type: int
+    W = int(next(tokens))  # type: int
+    s = [next(tokens) for _ in range(H)]  # type: "List[str]"
+    solve(H, W, s)
+    return
+
+main()
