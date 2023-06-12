ee434d82b1762ca169df2823f32279d4d335ffa7
masahiro-999 <masahiro999@outlook.com>
2023-06-12 20:21:23 +0900

    003 AC

--- typical90/003/main.py
+++ typical90/003/main.py
@@ -17,6 +17,29 @@ tokens = (i for line in iter(input, "") for i in line.split())
 
 
 def solve(N: int, A: "List[int]", B: "List[int]"):
+    def dfs(n,d,prev):
+        if g[n]:
+            ret = []
+            for n_next in g[n]:
+                if n_next == prev:
+                    continue
+                ret.append(dfs(n_next, d+1,n))
+            if not ret:
+                return (n,d)
+            ret.sort(key = lambda a:a[1])
+            return ret[-1]
+        else:
+            return (n,d)
+
+    g = defaultdict(list)
+    for a,b in zip(A,B):
+        g[a].append(b)
+        g[b].append(a)
+    
+    s = A[0]
+    (n1,_) = dfs(s, 0, None)
+    (n2,d) = dfs(n1, 0, None)
+    print(d+1)
 
 def main():
     N = int(next(tokens))  # type: int
