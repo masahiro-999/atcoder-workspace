6e7190ce4d546f6ce15f081e3da3c2697285ca33
masahiro-999 <masahiro999@outlook.com>
2023-06-17 15:07:36 +0900

    008 AC

--- typical90/008/main.py
+++ typical90/008/main.py
@@ -18,6 +18,20 @@ tokens = (i for line in iter(input, "") for i in line.split())
 MOD = 1000000007
 
 def solve(N: int, S: str):
+    str="atcoder"
+    d = [[0]*7 for _ in range(N)]
+    s = [x for x in S if x in str]
+    for i in range(len(s)-1,-1,-1):
+        if i != len(s)-1:
+            d[i] = d[i+1][:]
+        x = str.find(s[i])
+        if x == 6:
+            d[i][x] += 1
+
+        else:
+            d[i][x] += d[i][x+1]
+        d[i][x] %= MOD
+    print(d[0][0])
 
 def main():
     N = int(next(tokens))  # type: int
