1e37621195c546db5e397e63e44626cc2e45ced4
masahiro-999 <masahiro999@outlook.com>
2023-06-24 16:57:50 +0900

    013 ac

--- typical90/013/main.py
+++ typical90/013/main.py
@@ -18,6 +18,39 @@ tokens = (i for line in iter(input, "") for i in line.split())
 
 def solve(N: int, M: int, A: "List[int]", B: "List[int]", C: "List[int]"):
 
+    def bfs(s,t):
+        q = [(0,s)]
+        t[s] = 0
+        heapify(q)
+        visited = defaultdict(bool)
+        while q:
+            (d,p) = heappop(q)
+            if visited[p] == True:
+                continue
+            visited[p] = True
+            for next_p,c in u[p]:
+                if t[next_p] > d + c:
+                    # 新しいパスの方が近い
+                    t[next_p] = d + c
+                    heappush(q, (t[next_p], next_p))
+
+    u = defaultdict(list)
+    for a,b,c in zip(A,B,C):
+        a -= 1
+        b -= 1
+        u[a].append((b,c))
+        u[b].append((a,c))
+
+    t1 = [inf]*N
+    tn = [inf]*N
+
+    bfs(0,t1)
+    bfs(N-1,tn)
+
+    for i in range(N):
+        ans = t1[i] +tn[i]
+        print(ans)
+
 def main():
     N = int(next(tokens))  # type: int
     M = int(next(tokens))  # type: int
