bcfc60cf689b527b07006f50e096de25d4834732
masahiro-999 <masahiro999@outlook.com>
2023-06-25 17:28:15 +0900

    015 TLE

--- typical90/015/main.py
+++ typical90/015/main.py
@@ -17,7 +17,105 @@ tokens = (i for line in iter(input, "") for i in line.split())
 
 MOD = 1000000007
 
+class ModInt:
+    def __init__(self, x):
+        self.x = x % MOD
+
+    def __str__(self):
+        return str(self.x)
+
+    __repr__ = __str__
+
+    def __add__(self, other):
+        return (
+            ModInt(self.x + other.x) if isinstance(other, ModInt) else
+            ModInt(self.x + other)
+        )
+
+    def __sub__(self, other):
+        return (
+            ModInt(self.x - other.x) if isinstance(other, ModInt) else
+            ModInt(self.x - other)
+        )
+
+    def __mul__(self, other):
+        return (
+            ModInt(self.x * other.x) if isinstance(other, ModInt) else
+            ModInt(self.x * other)
+        )
+
+    def __truediv__(self, other):
+        return (
+            ModInt(
+                self.x * pow(other.x, MOD - 2, MOD)
+            ) if isinstance(other, ModInt) else
+            ModInt(self.x * pow(other, MOD - 2, MOD))
+        )
+
+    def __pow__(self, other):
+        return (
+            ModInt(pow(self.x, other.x, MOD)) if isinstance(other, ModInt) else
+            ModInt(pow(self.x, other, MOD))
+        )
+
+    __radd__ = __add__
+
+    def __rsub__(self, other):
+        return (
+            ModInt(other.x - self.x) if isinstance(other, ModInt) else
+            ModInt(other - self.x)
+        )
+
+    __rmul__ = __mul__
+
+    def __rtruediv__(self, other):
+        return (
+            ModInt(
+                other.x * pow(self.x, MOD - 2, MOD)
+            ) if isinstance(other, ModInt) else
+            ModInt(other * pow(self.x, MOD - 2, MOD))
+        )
+
+    def __rpow__(self, other):
+        return (
+            ModInt(pow(other.x, self.x, MOD)) if isinstance(other, ModInt) else
+            ModInt(pow(other, self.x, MOD))
+        )
+
+def prepare(n, MOD):
+    f = 1
+    factorials = [1]
+    for m in range(1, n + 1):
+        f *= m
+        f %= MOD
+        factorials.append(f)
+    inv = pow(f, MOD - 2, MOD)
+    invs = [1] * (n + 1)
+    invs[n] = inv
+    for m in range(n, 1, -1):
+        inv *= m
+        inv %= MOD
+        invs[m - 1] = inv
+ 
+    return factorials, invs
+
+def choose(n,a):
+    ret = ModInt(1)
+    for i in range(n,n-a, -1):
+        ret *= i
+    for i in range(1,a+1):
+        ret /= i
+    return ret
+
 def solve(N: int):
+    for k in range(1,N+1):
+        ans = ModInt(0)
+        for a in range(1,N+1):
+            x = N-(k-1)*(a-1)
+            if x <=0:
+                break
+            ans += choose(x,a)
+        print(ans)
 
 def main():
     N = int(next(tokens))  # type: int
