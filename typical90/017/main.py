f3586c20d288c46c9a4b1d2ae9d231de1138d30d
masahiro-999 <masahiro999@outlook.com>
2023-06-27 08:27:13 +0900

    017-作りかけ

--- typical90/017/main.py
+++ typical90/017/main.py
@@ -17,21 +17,50 @@ tokens = (i for line in iter(input, "") for i in line.split())
 
 
 def solve(N: int, M: int, L: "List[int]", R: "List[int]"):
-    def is_cross(l1, r1, l2, r2):
-        if l1 in [l2,r2] or r1 in [l2,r2]:
-            return False
-        if l1 > r1:
-            l1,r1 = r1,l1
-        if l2 > r2:
-            l2,r2 = r2,l2
-        return (l1 < l2 < r1)  != (l1 < r2 < r1) 
+    def create_c(l):
+        ret = []
+        count = 0
+        prev_i = 0
+        for i in l:
+            for _ in range(i-prev_i):
+                ret.append(count)
+            count += 1
+            prev_i = i
+        for _ in range(N-len(ret)):
+            ret.append(count)
+        return ret
+    
+    def count_between(c,i,j):
+        if i > j:
+            i,j = j,i
+            # i<j
+        return c[j-1] - c[i]
 
+    def count_cross(i,j):
+        # i<j
+        i_end_list = d[i]
+        j_end_list = d[j]
+        ret = 0
+        for i_end in i_end_list:
+            ret += count_between(c_table[j],i,i_end)
+        return ret
+
+    d = defaultdict(list)
+    c_table = [None]*N
+    for l,r in zip(L,R):
+        l -= 1
+        r -= 1
+        d[l].append(r)
+        d[r].append(l)
+    for i in range(N):
+        d[i].sort()
+        c_table[i] = create_c(d[i])
     ans = 0
-    for i in range(M-1):
-        for j in range(i+1,M):
-            if is_cross(L[i],R[i],L[j],R[j]):
-                ans += 1
+    for i in range(N-1):
+        for j in range(i+1,N):
+            ans += count_cross(i,j)
     print(ans)
+
 def main():
     N = int(next(tokens))  # type: int
     M = int(next(tokens))  # type: int
