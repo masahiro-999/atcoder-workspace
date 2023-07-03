053e1ff8e59435a7f046aec2839da9b47301815e
masahiro-999 <masahiro999@outlook.com>
2023-07-03 19:31:53 +0900

    019 作成中

--- typical90/019/main.py
+++ typical90/019/main.py
@@ -15,19 +15,37 @@ li = lambda: list(mi())
 inf = 2 ** 63 - 1
 tokens = (i for line in iter(input, "") for i in line.split())
 
+memo = {}
+import json
+
+c1 = 0
+c2 = 0
+
+def get_ans(a):
+    global c1,c2
+    c1 += 1
+    a_key=json.dumps(a)
+    r = memo.get(a_key,None)
+    if r is None:
+        c2 += 1
+        if a ==[]:
+            return 0
+        result = []
+        for i in range(len(a)-1):
+            b = a[:i]+a[i+2:]
+            d = abs(a[i]-a[i+1])
+            result.append(get_ans(b)+d)
+        r = min(result)
+        memo[a_key] = r
+    return r
 
 def solve(N: int, A: "List[int]"):
+    global c1,c2
 
-    a = A[:]
-    sum_d = 0
-    while a:
-        d = [abs(a[i]-a[i+1]) for i in range(len(a)-1)]
-        min_d = min(d)
-        sum_d += min_d
-        min_i = d.index(min_d)
-        a = a[:min_i] + a[min_i+2:]
-
-    print(sum_d)
+    ans = get_ans(A[:])
+    print(ans)
+    print(c1,c2)
+    print(len(memo))
 
 def main():
     N = int(next(tokens))  # type: int
