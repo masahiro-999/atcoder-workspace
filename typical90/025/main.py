371a0ef5569733fb27075df46e5498cad60481ab
masahiro-999 <masahiro999@outlook.com>
2023-07-08 15:42:37 +0900

    typical90 025

--- typical90/025/main.py
+++ typical90/025/main.py
@@ -16,7 +16,34 @@ inf = 2 ** 63 - 1
 tokens = (i for line in iter(input, "") for i in line.split())
 
 
+def f(x):
+    ret = 1
+    for i in x:
+        ret *= int(i)
+    return ret
+
+def str_cmp(str1, str2):
+    # str2はソート済み
+    s1 =[i for i in str1]
+    s1.sort()
+    s2 =[i for i in str2]
+    return s1 == s2
+
 def solve(N: int, B: int):
+    ans = 0
+    def create_num_and_count(str, i):
+        nonlocal ans
+        if len(str) == 11:
+            return
+        for j in range(i,9+1):
+            str_next = str+f'{j}'
+            # print(str_next)
+            m = B+f(str_next)
+            if 1 <= m <= N and str_cmp(f'{m}', str_next):  
+                ans += 1
+            create_num_and_count(str_next,j) 
+    create_num_and_count("",0)
+    print(ans)
 
 def main():
     N = int(next(tokens))  # type: int
