9525a4f98f3a0a90065aa2ca620294b603c92e95
masahiro-999 <masahiro999@outlook.com>
2023-07-20 21:00:22 +0900

    agc015_b

---
+++ agc015/B/main.py
@@ -0,0 +1,39 @@
+import sys, re
+from math import ceil, floor, sqrt, pi, factorial, gcd,sin,cos,tan,asin,acos,atan2,exp,log,log10
+from collections import deque, Counter, defaultdict
+from itertools import product, accumulate
+from functools import reduce,lru_cache
+from bisect import bisect
+from heapq import heapify, heappop, heappush
+sys.setrecursionlimit(5 * 10 ** 5)
+try:
+    from pypyjit import set_param
+    set_param('max_unroll_recursion=-1')
+except ModuleNotFoundError:
+    pass
+input = lambda: sys.stdin.readline().rstrip()
+ii = lambda: int(input())
+mi = lambda: map(int, input().split())
+li = lambda: list(mi())
+inf = 2 ** 63 - 1
+tokens = (i for line in iter(input, "") for i in line.split())
+
+
+def solve(s):
+    n = len(s)
+    ans = 0
+    for i in range(n):
+        u = n-i-1
+        d = i
+        if s[i] == "U":
+            ans += u+2*d
+        else:
+            ans += d+2*u
+    print(ans)
+
+def main():
+    s = input()
+    solve(s)
+    return
+
+main()
