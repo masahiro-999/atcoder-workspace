786cd426bcb09d9099f7f31d8dbf71420a46f9c3
masahiro-999 <masahiro999@outlook.com>
2023-07-21 14:23:55 +0900

    abc130_c

---
+++ abc130/C/main.py
@@ -0,0 +1,36 @@
+import sys, re
+from math import ceil, floor, sqrt, pi, factorial, gcd,sin,cos,tan,asin,acos,atan2,exp,log,log10
+from collections import deque, Counter, defaultdict
+from itertools import product, accumulate
+from functools import reduce,lru_cache
+from bisect import bisect
+from heapq import heapify, heappop, heappush
+sys.setrecursionlimit(5 * 10 ** 5)
+try:
+    from pypyjit import set_param
+    set_param('max_unroll_recursion=-1')
+except ModuleNotFoundError:
+    pass
+input = lambda: sys.stdin.readline().rstrip()
+ii = lambda: int(input())
+mi = lambda: map(int, input().split())
+li = lambda: list(mi())
+inf = 2 ** 63 - 1
+tokens = (i for line in iter(input, "") for i in line.split())
+
+
+def solve(W: int, H: int, x: int, y: int):
+    if x == W/2 and y == H/2:
+        print(W*H/2, 1)
+    else:
+        print(W*H/2, 0)
+
+def main():
+    W = int(next(tokens))  # type: int
+    H = int(next(tokens))  # type: int
+    x = int(next(tokens))  # type: int
+    y = int(next(tokens))  # type: int
+    solve(W, H, x, y)
+    return
+
+main()
