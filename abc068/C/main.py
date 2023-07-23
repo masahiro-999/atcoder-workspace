35614fb15c393518daa99282bb99ed319a2a7659
masahiro-999 <masahiro999@outlook.com>
2023-07-23 23:31:24 +0900

    abc068_c

---
+++ abc068/C/main.py
@@ -0,0 +1,46 @@
+import sys, re
+from math import ceil, floor, sqrt, pi, factorial, gcd,sin,cos,tan,asin,acos,atan2,exp,log,log10
+from collections import deque, Counter, defaultdict
+from itertools import product, accumulate
+from functools import reduce,lru_cache
+from bisect import bisect
+from heapq import heapify, heappop, heappush
+sys.setrecursionlimit(5 * 10 ** 5)
+try:
+    from pypyjit import set_param
+    set_param('max_unroll_recursion=-1')
+except ModuleNotFoundError:
+    pass
+input = lambda: sys.stdin.readline().rstrip()
+ii = lambda: int(input())
+mi = lambda: map(int, input().split())
+li = lambda: list(mi())
+inf = 2 ** 63 - 1
+tokens = (i for line in iter(input, "") for i in line.split())
+
+YES = "POSSIBLE"
+NO = "IMPOSSIBLE"
+
+def solve(N: int, M: int, A: "List[int]", B: "List[int]"):
+    g = defaultdict(set)
+    for a,b in zip(A,B):
+        g[a].add(b)
+        g[b].add(a)
+    
+    for i in range(1,N+1):
+        if 1 in g[i] and N in g[i]:
+            return YES
+    return NO
+
+def main():
+    N = int(next(tokens))  # type: int
+    M = int(next(tokens))  # type: int
+    a = [int()] * (M)  # type: "List[int]"
+    b = [int()] * (M)  # type: "List[int]"
+    for i in range(M):
+        a[i] = int(next(tokens))
+        b[i] = int(next(tokens))
+    print(solve(N, M, a, b))
+    return
+
+main()
