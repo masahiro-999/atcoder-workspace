328d36a546fdc6db0d7636732afc67d31e597250
masahiro-999 <masahiro999@outlook.com>
2023-07-27 10:23:45 +0900

    agc009_a

---
+++ agc009/A/main.py
@@ -0,0 +1,39 @@
+import sys, re
+from math import ceil, floor, sqrt, pi, factorial, gcd,sin,cos,tan,asin,acos,atan2,exp,log,log10
+from collections import deque, Counter, defaultdict
+from itertools import product, accumulate
+from functools import reduce,lru_cache
+from bisect import bisect
+from heapq import heapify, heappop, heappush
+sys.setrecursionlimit(5 * 10 ** 5)
+try:
+    from pypyjit import set_param
+    set_param('max_unroll_recursion=-1')
+except ModuleNotFoundError:
+    pass
+input = lambda: sys.stdin.readline().rstrip()
+ii = lambda: int(input())
+mi = lambda: map(int, input().split())
+li = lambda: list(mi())
+inf = 2 ** 63 - 1
+tokens = (i for line in iter(input, "") for i in line.split())
+
+
+def solve(N: int, A: "List[int]", B: "List[int]"):
+    ans = 0
+    for i in range(N-1,-1,-1):
+        if (A[i]+ans) % B[i] > 0:
+            ans += B[i] - (A[i]+ans) % B[i]
+    print(ans)
+
+def main():
+    N = int(next(tokens))  # type: int
+    A = [int()] * (N)  # type: "List[int]"
+    B = [int()] * (N)  # type: "List[int]"
+    for i in range(N):
+        A[i] = int(next(tokens))
+        B[i] = int(next(tokens))
+    solve(N, A, B)
+    return
+
+main()
