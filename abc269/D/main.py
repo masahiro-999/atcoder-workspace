1275cf6cc9dbcfafd694ed304fcd2ed957c57313
masahiro-999 <masahiro999@outlook.com>
2023-08-14 01:59:44 +0900

    abc269_d

---
+++ abc269/D/main.py
@@ -0,0 +1,97 @@
+import sys, re
+from math import ceil, floor, sqrt, pi, factorial, gcd,sin,cos,tan,asin,acos,atan2,exp,log,log10
+from collections import deque, Counter, defaultdict
+from itertools import product, accumulate
+from functools import reduce,lru_cache
+from bisect import bisect
+from heapq import heapify, heappop, heappush
+sys.setrecursionlimit(5 * 10 ** 5)
+try:
+    from pypyjit import set_param
+    set_param('max_unroll_recursion=-1')
+except ModuleNotFoundError:
+    pass
+input = lambda: sys.stdin.readline().rstrip()
+ii = lambda: int(input())
+mi = lambda: map(int, input().split())
+li = lambda: list(mi())
+inf = 2 ** 63 - 1
+tokens = (i for line in iter(input, "") for i in line.split())
+
+class UnionFind():
+    def __init__(self, n):
+        self.n = n
+        self.parents = [-1] * n
+
+    def find(self, x):
+        if self.parents[x] < 0:
+            return x
+        else:
+            self.parents[x] = self.find(self.parents[x])
+            return self.parents[x]
+
+    def union(self, x, y):
+        x = self.find(x)
+        y = self.find(y)
+
+        if x == y:
+            return
+
+        if self.parents[x] > self.parents[y]:
+            x, y = y, x
+
+        self.parents[x] += self.parents[y]
+        self.parents[y] = x
+
+    def size(self, x):
+        return -self.parents[self.find(x)]
+
+    def same(self, x, y):
+        return self.find(x) == self.find(y)
+
+    def members(self, x):
+        root = self.find(x)
+        return [i for i in range(self.n) if self.find(i) == root]
+
+    def roots(self):
+        return [i for i, x in enumerate(self.parents) if x < 0]
+
+    def group_count(self):
+        return len(self.roots())
+
+    def all_group_members(self):
+        group_members = defaultdict(list)
+        for member in range(self.n):
+            group_members[self.find(member)].append(member)
+        return group_members
+
+    def __str__(self):
+        return '\n'.join(f'{r}: {m}' for r, m in self.all_group_members().items())
+
+def solve(N: int, X: "List[int]", Y: "List[int]"):
+    dxdy = ([-1,0],[1,0],[0,-1],[0,1],[1,1],[-1,-1])
+    d = defaultdict(lambda : -1)
+    u = UnionFind(N)
+
+    for i,(x,y) in enumerate(zip(X,Y)):
+        d[(x,y)] = i
+
+    for i,(x,y) in enumerate(zip(X,Y)):
+        for dx,dy in dxdy:
+            j = d[(x+dx, y+dy)]
+            if j != -1:
+                u.union(i,j)
+    ans = u.group_count()
+    print(ans)
+
+def main():
+    N = int(next(tokens))  # type: int
+    X = [int()] * (N)  # type: "List[int]"
+    Y = [int()] * (N)  # type: "List[int]"
+    for i in range(N):
+        X[i] = int(next(tokens))
+        Y[i] = int(next(tokens))
+    solve(N, X, Y)
+    return
+
+main()
