a76a8384bfaf9595eb2cde49f8bd20bcdd944fe5
masahiro-999 <masahiro999@outlook.com>
2023-09-30 17:29:14 +0900

    abc194_d

---
+++ abc194/D/main.py
@@ -0,0 +1,37 @@
+import sys, re
+from math import ceil, floor, sqrt, pi, factorial, gcd,sin,cos,tan,asin,acos,atan2,exp,log,log10
+from collections import deque, Counter, defaultdict
+from itertools import product, accumulate
+from functools import reduce,lru_cache
+from bisect import bisect
+from heapq import heapify, heappop, heappush
+sys.setrecursionlimit(5 * 10 ** 5)
+try:
+    from pypyjit import set_param
+    set_param('max_unroll_recursion=-1')
+except ModuleNotFoundError:
+    pass
+input = lambda: sys.stdin.readline().rstrip()
+ii = lambda: int(input())
+mi = lambda: map(int, input().split())
+li = lambda: list(mi())
+inf = 2 ** 63 - 1
+tokens = (i for line in iter(input, "") for i in line.split())
+
+
+def solve(N: int):
+    def f(n):
+        return N/(N-n)
+    
+    ans = 0
+    for i in range(1,N):
+        ans += f(i)
+
+    print(ans)
+
+def main():
+    N = int(next(tokens))  # type: int
+    solve(N)
+    return
+
+main()
