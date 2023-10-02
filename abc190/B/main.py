5d42abbb6e891f26762633598a9ca7bc94720946
masahiro-999 <masahiro999@outlook.com>
2023-10-02 22:25:54 +0900

    abc190_b

---
+++ abc190/B/main.py
@@ -0,0 +1,44 @@
+import sys, re
+from math import ceil, floor, sqrt, pi, factorial, gcd,sin,cos,tan,asin,acos,atan2,exp,log,log10
+from collections import deque, Counter, defaultdict
+from itertools import product, accumulate
+from functools import reduce,lru_cache
+from bisect import bisect
+from heapq import heapify, heappop, heappush
+sys.setrecursionlimit(5 * 10 ** 5)
+try:
+    from pypyjit import set_param
+    set_param('max_unroll_recursion=-1')
+except ModuleNotFoundError:
+    pass
+input = lambda: sys.stdin.readline().rstrip()
+ii = lambda: int(input())
+mi = lambda: map(int, input().split())
+li = lambda: list(mi())
+inf = 2 ** 63 - 1
+tokens = (i for line in iter(input, "") for i in line.split())
+
+YES = "Yes"
+NO = "No"
+
+def solve(N: int, S: int, D: int, X: "List[int]", Y: "List[int]"):
+    ans = NO
+    for x,y in zip(X,Y):
+        if x < S and y > D:
+            ans = YES
+            break
+    print(ans)
+
+def main():
+    N = int(next(tokens))  # type: int
+    S = int(next(tokens))  # type: int
+    D = int(next(tokens))  # type: int
+    X = [int()] * (N)  # type: "List[int]"
+    Y = [int()] * (N)  # type: "List[int]"
+    for i in range(N):
+        X[i] = int(next(tokens))
+        Y[i] = int(next(tokens))
+    solve(N, S, D, X, Y)
+    return
+
+main()
