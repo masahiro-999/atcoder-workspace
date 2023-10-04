78e8b1bff7232f90a319a04073b70576e1e7fdb2
masahiro-999 <masahiro999@outlook.com>
2023-10-04 21:49:06 +0900

    abc188_d

---
+++ abc188/D/main.py
@@ -0,0 +1,60 @@
+import sys, re
+from math import ceil, floor, sqrt, pi, factorial, gcd,sin,cos,tan,asin,acos,atan2,exp,log,log10
+from collections import deque, Counter, defaultdict
+from itertools import product, accumulate
+from functools import reduce,lru_cache
+from bisect import bisect
+from heapq import heapify, heappop, heappush
+sys.setrecursionlimit(5 * 10 ** 5)
+try:
+    from pypyjit import set_param
+    set_param('max_unroll_recursion=-1')
+except ModuleNotFoundError:
+    pass
+input = lambda: sys.stdin.readline().rstrip()
+ii = lambda: int(input())
+mi = lambda: map(int, input().split())
+li = lambda: list(mi())
+inf = 2 ** 63 - 1
+tokens = (i for line in iter(input, "") for i in line.split())
+
+
+def solve(N: int, C: int, a: "List[int]", b: "List[int]", c: "List[int]"):
+    active=set()
+    sum_active = 0
+    event = []
+    for i,x in enumerate(a):
+        event.append((x,False,i)) 
+    for i,x in enumerate(b):
+        event.append((x,True,i))
+    event.sort()
+    ans = 0
+    prev_x = 0
+    for x,is_end,i in event:
+        if not is_end:
+            ans += min(C, sum_active)*((x-1)-prev_x+1)
+            active.add(i)
+            sum_active += c[i]
+            prev_x = x
+        else:
+            # end
+            ans += min(C, sum_active)*(x-prev_x+1)
+            active.remove(i)
+            sum_active -= c[i]
+            prev_x = x+1
+    print(ans)
+
+def main():
+    N = int(next(tokens))  # type: int
+    C = int(next(tokens))  # type: int
+    a = [int()] * (N)  # type: "List[int]"
+    b = [int()] * (N)  # type: "List[int]"
+    c = [int()] * (N)  # type: "List[int]"
+    for i in range(N):
+        a[i] = int(next(tokens))
+        b[i] = int(next(tokens))
+        c[i] = int(next(tokens))
+    solve(N, C, a, b, c)
+    return
+
+main()
