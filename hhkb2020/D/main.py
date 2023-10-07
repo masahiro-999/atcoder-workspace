6719bdee7a39084caae0a88466dabb4c60f6dda6
masahiro-999 <masahiro999@outlook.com>
2023-10-07 16:02:44 +0900

    hhkb2202_d 別解追加

--- hhkb2020/D/main.py
+++ hhkb2020/D/main.py
@@ -20,7 +20,7 @@ tokens = (i for line in iter(input, "") for i in line.split())
 
 MOD = 1000000007
 
-def solve(T,t):
+def solve2(T,t):
     def p(n,x):
         # 幅nの中に幅xを置く場合の数
         return n-x+1 % MOD
@@ -35,6 +35,28 @@ def solve(T,t):
             ans1 = p(N,A)*p(N,B)*sum_n(1,N-A-B+1)*4
             ans2 = sum_n(1,N-A-B+1)**2*4
             ans = (ans1 - ans2) % MOD
+        print(ans)
+
+def solve(T,t):
+    def p(n,x):
+        # 幅nの中に幅xを置く場合の数
+        return n-x+1
+    def sum_n(a,n):
+        return (2*a-(n-1))*n//2
+
+    for N,A,B in t:
+        if N < A+B:
+            ans = 0
+        else:
+            if A<B:
+                A,B = B,A
+            ans = pow(p(N,A),2,MOD) * pow(p(N,B),2,MOD)
+            ans %= MOD
+            ans -= pow(sum_n(p(N,A+1),B-1)*2,2,MOD)
+            ans -= sum_n(p(N,A+1),B-1)*2*p(N,A)*p(A,B)*2
+            ans %= MOD
+            ans -= pow(p(N,A),2,MOD)*pow(p(A,B),2,MOD)
+            ans %= MOD
         print(ans)
 
 def main():
