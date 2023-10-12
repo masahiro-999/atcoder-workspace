0e2e1affecece96ddf8d1eb319479668bb871454
masahiro-999 <masahiro999@outlook.com>
2023-10-13 08:23:22 +0900

    abc169_d

---
+++ abc169/D/main.py
@@ -0,0 +1,151 @@
+from io import BytesIO, IOBase
+
+import sys
+import os
+
+from math import ceil, floor, sqrt, pi, factorial, gcd,lcm,sin,cos,tan,asin,acos,atan2,exp,log,log10
+from bisect import bisect, bisect_left, bisect_right
+from collections import Counter, defaultdict, deque
+from copy import deepcopy
+from functools import cmp_to_key, lru_cache, reduce
+from heapq import heapify, heappop, heappush, heappushpop, nlargest, nsmallest
+from itertools import product, accumulate,permutations,combinations, count
+from operator import add, iand, ior, itemgetter, mul, xor
+from string import ascii_lowercase, ascii_uppercase, ascii_letters
+from typing import *
+
+BUFSIZE = 4096
+
+class FastIO(IOBase):
+    newlines = 0
+
+    def __init__(self, file):
+        self._fd = file.fileno()
+        self.buffer = BytesIO()
+        self.writable = "x" in file.mode or "r" not in file.mode
+        self.write = self.buffer.write if self.writable else None
+
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            if not b:
+                break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
+
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            self.newlines = b.count(b"\n") + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
+
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
+
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
+
+sys.stdin = IOWrapper(sys.stdin)
+sys.stdout = IOWrapper(sys.stdout)
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+
+if True:
+    def I():
+        return input()
+
+    def II():
+        return int(input())
+
+    def MII():
+        return map(int, input().split())
+
+    def LI():
+        return list(input().split())
+
+    def LII():
+        return list(map(int, input().split()))
+
+    def TII():
+        return tuple(map(int, input().split()))
+
+    def GMI():
+        return map(lambda x: int(x) - 1, input().split())
+
+    def LGMI():
+        return list(map(lambda x: int(x) - 1, input().split()))
+
+sys.setrecursionlimit(5 * 10 ** 5)
+try:
+    from pypyjit import set_param
+    set_param('max_unroll_recursion=-1')
+except ModuleNotFoundError:
+    pass
+
+def get_prime_list(num_max):
+    prime_table=[1]*(num_max+1)
+    prime_table[0] = 0
+    prime_table[1] = 0
+    for i in range(2, num_max+1):
+        k = i*2
+        while k <= num_max:
+            prime_table[k] = 0
+            k += i
+    return [i for i in range(2,num_max+1) if prime_table[i]]
+
+def f(x):
+    return x*(x+1)//2
+
+def dsum(n):
+    if n == 1:
+        return 1
+    l = 1
+    r = n
+    while r-l > 1:
+        mid = (r+l)//2
+        if f(mid) <=n:
+            l = mid
+        else:
+            r = mid
+    return l
+
+N = II()  # type: int
+
+prime_list = get_prime_list(1000000)
+
+n = N
+
+num_factor = []
+for p in prime_list:
+    if n % p == 0:
+        cnt = 0
+        while  n % p == 0:
+                n //= p
+                cnt += 1
+        num_factor.append(cnt)
+if n > 1:
+    num_factor.append(1)
+
+if len(num_factor) == 0:
+    if N == 1:
+        ans = 0
+    else:
+        ans = 1
+else:
+    ans = 0
+    for c in num_factor:
+        ans += dsum(c)
+
+print(ans)
