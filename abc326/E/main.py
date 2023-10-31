2dd3035eb837f73b37ac269346df638ae805a76c
masahiro-999 <masahiro999@outlook.com>
2023-10-31 20:32:30 +0900

    abc326_e

---
+++ abc326/E/main.py
@@ -0,0 +1,203 @@
+from io import BytesIO, IOBase
+
+import sys
+import os
+
+from math import ceil, floor, sqrt, pi, factorial, gcd,lcm,sin,cos,tan,asin,acos,atan2,exp,log,log10
+from bisect import bisect, bisect_left, bisect_right
+from collections import Counter, defaultdict, deque
+from copy import deepcopy
+from functools import cmp_to_key, lru_cache, reduce
+from heapq import heapify, heappop, heappush, heappushpop, nlargest, nsmallest
+from itertools import product, accumulate,permutations,combinations, count
+from operator import add, iand, ior, itemgetter, mul, xor
+from string import ascii_lowercase, ascii_uppercase, ascii_letters
+from typing import *
+
+BUFSIZE = 4096
+
+class FastIO(IOBase):
+    newlines = 0
+
+    def __init__(self, file):
+        self._fd = file.fileno()
+        self.buffer = BytesIO()
+        self.writable = "x" in file.mode or "r" not in file.mode
+        self.write = self.buffer.write if self.writable else None
+
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            if not b:
+                break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
+
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            self.newlines = b.count(b"\n") + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
+
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
+
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
+
+sys.stdin = IOWrapper(sys.stdin)
+sys.stdout = IOWrapper(sys.stdout)
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+
+if True:
+    def I():
+        return input()
+
+    def II():
+        return int(input())
+
+    def MII():
+        return map(int, input().split())
+
+    def LI():
+        return list(input().split())
+
+    def LII():
+        return list(map(int, input().split()))
+
+    def TII():
+        return tuple(map(int, input().split()))
+
+    def GMI():
+        return map(lambda x: int(x) - 1, input().split())
+
+    def LGMI():
+        return list(map(lambda x: int(x) - 1, input().split()))
+
+sys.setrecursionlimit(5 * 10 ** 5)
+try:
+    from pypyjit import set_param
+    set_param('max_unroll_recursion=-1')
+except ModuleNotFoundError:
+    pass
+
+MOD = 998244353
+
+# https://qiita.com/wotsushi/items/c936838df992b706084c
+# abc298 D
+# abc156 D
+# abc145 D
+# で使った
+
+class ModInt:
+    def __init__(self, x):
+        self.x = x % MOD
+
+    def __str__(self):
+        return str(self.x)
+
+    __repr__ = __str__
+
+    def __add__(self, other):
+        return (
+            ModInt(self.x + other.x) if isinstance(other, ModInt) else
+            ModInt(self.x + other)
+        )
+
+    def __sub__(self, other):
+        return (
+            ModInt(self.x - other.x) if isinstance(other, ModInt) else
+            ModInt(self.x - other)
+        )
+
+    def __mul__(self, other):
+        return (
+            ModInt(self.x * other.x) if isinstance(other, ModInt) else
+            ModInt(self.x * other)
+        )
+
+    def __truediv__(self, other):
+        return (
+            ModInt(
+                self.x * pow(other.x, MOD - 2, MOD)
+            ) if isinstance(other, ModInt) else
+            ModInt(self.x * pow(other, MOD - 2, MOD))
+        )
+
+    def __pow__(self, other):
+        return (
+            ModInt(pow(self.x, other.x, MOD)) if isinstance(other, ModInt) else
+            ModInt(pow(self.x, other, MOD))
+        )
+
+    __radd__ = __add__
+
+    def __rsub__(self, other):
+        return (
+            ModInt(other.x - self.x) if isinstance(other, ModInt) else
+            ModInt(other - self.x)
+        )
+
+    __rmul__ = __mul__
+
+    def __rtruediv__(self, other):
+        return (
+            ModInt(
+                other.x * pow(self.x, MOD - 2, MOD)
+            ) if isinstance(other, ModInt) else
+            ModInt(other * pow(self.x, MOD - 2, MOD))
+        )
+
+    def __rpow__(self, other):
+        return (
+            ModInt(pow(other.x, self.x, MOD)) if isinstance(other, ModInt) else
+            ModInt(pow(other, self.x, MOD))
+        )
+
+def prepare(n, MOD):
+    f = 1
+    factorials = [1]
+    for m in range(1, n + 1):
+        f *= m
+        f %= MOD
+        factorials.append(f)
+    inv = pow(f, MOD - 2, MOD)
+    invs = [1] * (n + 1)
+    invs[n] = inv
+    for m in range(n, 1, -1):
+        inv *= m
+        inv %= MOD
+        invs[m - 1] = inv
+ 
+    return factorials, invs
+
+factorials, invs = prepare(100000,MOD)
+
+def choose(n,a):
+    return (factorials[n] * (invs[n-a] * invs[a])) % MOD
+
+
+N = II()  # type: int
+A=LII()
+sum_e = ModInt(0)
+e = [0]*N
+for i in range(N-1,-1,-1):
+    e[i] = A[i] + sum_e /N
+    sum_e += e[i]
+
+# ans = sum(e) / ModInt(N)
+ans = sum_e / ModInt(N)
+print(ans)
