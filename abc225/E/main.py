a3c46ed5c4fd885c21208bc7d8bc2b70ee745dc5
masahiro-999 <masahiro999@outlook.com>
2024-01-02 22:08:48 +0900

    abc225_e

---
+++ abc225/E/main.py
@@ -0,0 +1,148 @@
+from io import BytesIO, IOBase
+
+import sys
+import os
+
+from math import ceil, floor, sqrt, pi, factorial, gcd,lcm,sin,cos,tan,asin,acos,atan2,exp,log,log10
+from bisect import bisect, bisect_left, bisect_right
+from collections import Counter, defaultdict, deque
+from copy import deepcopy
+from functools import cmp_to_key, lru_cache, reduce
+from heapq import heapify, heappop, heappush, heappushpop, nlargest, nsmallest
+from itertools import product, accumulate,permutations,combinations, count
+from operator import add, iand, ior, itemgetter, mul, xor
+from string import ascii_lowercase, ascii_uppercase, ascii_letters
+from typing import *
+from sortedcontainers import SortedSet, SortedList, SortedDict
+from fractions import Fraction
+
+BUFSIZE = 4096
+
+class FastIO(IOBase):
+    newlines = 0
+
+    def __init__(self, file):
+        self._fd = file.fileno()
+        self.buffer = BytesIO()
+        self.writable = "x" in file.mode or "r" not in file.mode
+        self.write = self.buffer.write if self.writable else None
+
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            if not b:
+                break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
+
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            self.newlines = b.count(b"\n") + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
+
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
+
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
+
+sys.stdin = IOWrapper(sys.stdin)
+sys.stdout = IOWrapper(sys.stdout)
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+
+if True:
+    def I():
+        return input()
+
+    def II():
+        return int(input())
+
+    def MII():
+        return map(int, input().split())
+
+    def LI():
+        return list(input().split())
+
+    def LII():
+        return list(map(int, input().split()))
+
+    def TII():
+        return tuple(map(int, input().split()))
+
+    def GMI():
+        return map(lambda x: int(x) - 1, input().split())
+
+    def LGMI():
+        return list(map(lambda x: int(x) - 1, input().split()))
+
+sys.setrecursionlimit(5 * 10 ** 5)
+try:
+    from pypyjit import set_param
+    set_param('max_unroll_recursion=-1')
+except ModuleNotFoundError:
+    pass
+
+inf = 1<<60
+
+# class Fraction:
+#     def __init__(self,a,b):
+#         # if a < 0:
+#         #     a,b = -a,-b
+#         # if b != 0:
+#         #     gcd_ab = gcd(a,b)
+#         #     a //= gcd_ab
+#         #     b //= gcd_ab
+#         # else:
+#         #     a = 1
+#         self.a = a
+#         self.b = b
+#     def __lt__(self, other):
+#         return other.b*self.a < other.a*self.b
+#     def __le__(self, other):
+#         return other.b*self.a <= other.a*self.b
+#     def __eq__(self, other):
+#         return other.b*self.a == other.a*self.b
+#     def __ne__(self, other):
+#         return other.b*self.a != other.a*self.b
+#     def __gt__(self, other):
+#         return other.b*self.a > other.a*self.b
+#     def __ge__(self, other):
+#         return other.b*self.a >= other.a*self.b
+
+from functools import cmp_to_key
+
+N = II()
+xy = [TII() for _ in range(N)]
+
+l = [((y, x-1), (y-1,x)) for x,y in xy]
+def cmp(a,b):
+    return b[1]*a[0] - b[0]*a[1]
+# print(xy)
+# print(l)
+l.sort(key=cmp_to_key(lambda x,y: cmp(x[0],y[0])))
+
+# print(l)
+
+ans = 0
+prev = (0,1)
+for a,b in l:
+    # print(a,b,prev)
+    if cmp(b,prev) >=0:
+        ans += 1
+        prev = a
+
+print(ans)
