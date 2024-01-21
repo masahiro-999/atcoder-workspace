52261da7562f3d880e5e4744fe8637ca8e6750d3
masahiro-999 <masahiro999@outlook.com>
2024-01-22 07:57:45 +0900

    dp_h

--- dp/H/main.py
+++ dp/H/main.py
@@ -98,3 +98,23 @@ except ModuleNotFoundError:
 inf = 1<<60
 MOD = 1000000007
 
+H,W = TII()
+A = [I() for _ in range(H)]
+
+dp = [[0]*W for _ in range(H)]
+
+dp[0][0]=1
+
+for i in range(H):
+    for j in range(W):
+        if A[i][j] == "#":
+            continue
+        if i > 0:
+            dp[i][j] +=dp[i-1][j]
+
+        if j > 0:
+            dp[i][j] +=dp[i][j-1]
+        dp[i][j] %= MOD
+
+ans = dp[H-1][W-1]
+print(ans)
