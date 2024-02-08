6670ffe4456b1fdc060b73ebab8a55617feab0e1
masahiro-999 <masahiro999@outlook.com>
2024-02-09 07:38:49 +0900

    abc312_e

---
+++ abc312/E/main.py
@@ -0,0 +1,130 @@
+from io import BytesIO, IOBase
+
+import sys
+import os
+
+from math import ceil, floor, sqrt, pi, factorial, gcd,lcm,sin,cos,tan,asin,acos,atan2,exp,log,log10
+from bisect import bisect, bisect_left, bisect_right
+from collections import Counter, defaultdict, deque
+from copy import deepcopy
+from functools import cmp_to_key, lru_cache, reduce, cache
+from heapq import heapify, heappop, heappush, heappushpop, nlargest, nsmallest
+from itertools import product, accumulate,permutations,combinations, count
+from operator import add, iand, ior, itemgetter, mul, xor
+from string import ascii_lowercase, ascii_uppercase, ascii_letters
+from typing import *
+from sortedcontainers import SortedSet, SortedList, SortedDict
+
+BUFSIZE = 4096
+
+class FastIO(IOBase):
+    newlines = 0
+
+    def __init__(self, file):
+        self._fd = file.fileno()
+        self.buffer = BytesIO()
+        self.writable = "x" in file.mode or "r" not in file.mode
+        self.write = self.buffer.write if self.writable else None
+
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            if not b:
+                break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
+
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            self.newlines = b.count(b"\n") + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
+
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
+
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
+
+sys.stdin = IOWrapper(sys.stdin)
+sys.stdout = IOWrapper(sys.stdout)
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+
+if True:
+    def I():
+        return input()
+
+    def II():
+        return int(input())
+
+    def MII():
+        return map(int, input().split())
+
+    def LI():
+        return list(input().split())
+
+    def LII():
+        return list(map(int, input().split()))
+
+    def TII():
+        return tuple(map(int, input().split()))
+
+    def GMI():
+        return map(lambda x: int(x) - 1, input().split())
+
+    def LGMI():
+        return list(map(lambda x: int(x) - 1, input().split()))
+
+sys.setrecursionlimit(5 * 10 ** 5)
+try:
+    from pypyjit import set_param
+    set_param('max_unroll_recursion=-1')
+except ModuleNotFoundError:
+    pass
+
+dxdy1 = ((0, 1), (0, -1), (1, 0), (-1, 0))  # 上下左右
+dxdy2 = ((0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (-1, -1), (1, -1), (-1, 1))  # 8方向すべて
+dxdy3 = ((0, 1), (1, 0))  # 右 or 下
+dxdy4 = ((1, 1), (1, -1), (-1, 1), (-1, -1))  # 斜め
+
+inf = 1<<60
+
+N = II()
+xyz = [TII() for _ in range(N)]
+
+t = [[[0]*100 for _ in range(100)] for _ in range(100)]
+
+for i,(x1,y1,z1,x2,y2,z2) in enumerate(xyz,start=1):
+    for x,y,z in product(range(x1,x2),range(y1,y2),range(z1,z2)):
+        t[x][y][z] = i
+
+ans_set = defaultdict(set)
+for x,y,z in product(range(100),repeat=3):
+    for dx,dy,dz in ((1,0,0),(-1,0,0),(0,1,0),(0,-1,0),(0,0,1),(0,0,-1)):
+        q1 = t[x][y][z]
+        if q1 == 0:
+            continue
+        if x+dx <0 or x+dx >= 100 or y+dy <0 or y+dy >= 100 or z+dz <0 or z+dz >= 100:
+            continue
+        q2 = t[x+dx][y+dy][z+dz]
+        if q2 == 0:
+            continue
+        if q1 != q2:
+            ans_set[q1].add(q2)
+            ans_set[q2].add(q1)
+
+for i in range(1,N+1):
+    print(len(ans_set[i]))
