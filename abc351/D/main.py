da440e8225cf5eb8ca0b54e4a9e7694a28e24bae
masahiro-999 <masahiro999@outlook.com>
2024-04-27 22:44:35 +0900

    abc351_abcd

---
+++ abc351/D/main.py
@@ -0,0 +1,176 @@
+from io import BytesIO, IOBase
+
+import sys
+import os
+
+from math import ceil, floor, sqrt, pi, factorial, gcd,lcm,sin,cos,tan,asin,acos,atan2,exp,log,log10
+from bisect import bisect, bisect_left, bisect_right
+from collections import Counter, defaultdict, deque
+from copy import deepcopy
+from functools import cmp_to_key, lru_cache, reduce, cache
+from heapq import heapify, heappop, heappush, heappushpop, nlargest, nsmallest
+from itertools import product, accumulate,permutations,combinations, count
+from operator import add, iand, ior, itemgetter, mul, xor
+from string import ascii_lowercase, ascii_uppercase, ascii_letters
+from typing import *
+from sortedcontainers import SortedSet, SortedList, SortedDict
+
+BUFSIZE = 4096
+
+class FastIO(IOBase):
+    newlines = 0
+
+    def __init__(self, file):
+        self._fd = file.fileno()
+        self.buffer = BytesIO()
+        self.writable = "x" in file.mode or "r" not in file.mode
+        self.write = self.buffer.write if self.writable else None
+
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            if not b:
+                break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
+
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            self.newlines = b.count(b"\n") + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
+
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
+
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
+
+sys.stdin = IOWrapper(sys.stdin)
+sys.stdout = IOWrapper(sys.stdout)
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+
+if True:
+    def I():
+        return input()
+
+    def II():
+        return int(input())
+
+    def MII():
+        return map(int, input().split())
+
+    def LI():
+        return list(input().split())
+
+    def LII():
+        return list(map(int, input().split()))
+
+    def TII():
+        return tuple(map(int, input().split()))
+
+    def GMI():
+        return map(lambda x: int(x) - 1, input().split())
+
+    def LGMI():
+        return list(map(lambda x: int(x) - 1, input().split()))
+
+sys.setrecursionlimit(5 * 10 ** 5)
+# try:
+#     from pypyjit import set_param
+#     set_param('max_unroll_recursion=-1')
+# except ModuleNotFoundError:
+#     pass
+
+dxdy1 = ((0, 1), (0, -1), (1, 0), (-1, 0))  # 上下左右
+dxdy2 = ((0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (-1, -1), (1, -1), (-1, 1))  # 8方向すべて
+dxdy3 = ((0, 1), (1, 0))  # 右 or 下
+dxdy4 = ((1, 1), (1, -1), (-1, 1), (-1, -1))  # 斜め
+
+inf = 1<<60
+
+H,W = LII()
+S = [I() for _ in range(H)]
+
+visited = [[False]*W for _ in range(H)]
+cnt = 0
+
+stop = [[False]*W for _ in range(H)]
+same = [[False]*W for _ in range(H)]
+
+for i in range(H):
+    for j in range(W):
+        if S[i][j] == "#":
+            for dx,dy in dxdy1:
+                ni,nj = i+dx,j+dy
+                if 0 <= ni < H and 0 <= nj < W and S[ni][nj] == ".":
+                    stop[ni][nj] = True
+
+from types import GeneratorType
+
+def bootstrap(f, stack=[]):
+    def wrappedfunc(*args, **kwargs):
+        if stack:
+            return f(*args, **kwargs)
+        else:
+            to = f(*args, **kwargs)
+            while True:
+                if type(to) is GeneratorType:
+                    stack.append(to)
+                    to = next(to)
+                else:
+                    stack.pop()
+                    if not stack:
+                        break
+                    to = stack[-1].send(to)
+            return to
+    return wrappedfunc
+
+@bootstrap
+def dfs(i,j):
+    # print(i,j)
+    global cnt,visited,same
+    cnt += 1
+    if stop[i][j]:
+        stop_set.add((i,j))
+        yield
+    visited[i][j] = True
+    for dx,dy in dxdy1:
+        ni,nj = i+dx,j+dy
+        if 0 <= ni < H and 0 <= nj < W and S[ni][nj] == ".":
+            # print("a",ni,nj)
+            if visited[ni][nj]:
+                continue
+            if (ni,nj) in stop_set:
+                continue
+            yield dfs(ni,nj)
+    yield
+
+ans = 1
+visited = [[False]*W for _ in range(H)]
+for i in range(H):
+    for j in range(W):
+        if stop[i][j]:
+            continue
+        if visited[i][j]:
+            continue
+        if S[i][j] == ".":
+            cnt = 0
+            stop_set = set()
+            dfs(i,j)
+            ans = max(ans,cnt)
+
+print(ans)
