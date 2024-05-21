d4dc5eb60b6460fa43fbab17a6a835d9d3b88ebf
masahiro-999 <masahiro999@outlook.com>
2024-05-21 21:13:12 +0900

    abc291_f

---
+++ abc291/F/main.py
@@ -0,0 +1,153 @@
+from io import BytesIO, IOBase
+
+import sys
+import os
+
+from math import ceil, floor, sqrt, pi, factorial, gcd,lcm,sin,cos,tan,asin,acos,atan2,exp,log,log10
+from bisect import bisect, bisect_left, bisect_right
+from collections import Counter, defaultdict, deque
+from copy import deepcopy
+from functools import cmp_to_key, lru_cache, reduce, cache
+from heapq import heapify, heappop, heappush, heappushpop, nlargest, nsmallest
+from itertools import product, accumulate,permutations,combinations, count
+from operator import add, iand, ior, itemgetter, mul, xor
+from string import ascii_lowercase, ascii_uppercase, ascii_letters
+from typing import *
+from sortedcontainers import SortedSet, SortedList, SortedDict
+
+BUFSIZE = 4096
+
+class FastIO(IOBase):
+    newlines = 0
+
+    def __init__(self, file):
+        self._fd = file.fileno()
+        self.buffer = BytesIO()
+        self.writable = "x" in file.mode or "r" not in file.mode
+        self.write = self.buffer.write if self.writable else None
+
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            if not b:
+                break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
+
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            self.newlines = b.count(b"\n") + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
+
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
+
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
+
+sys.stdin = IOWrapper(sys.stdin)
+sys.stdout = IOWrapper(sys.stdout)
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+
+if True:
+    def I():
+        return input()
+
+    def II():
+        return int(input())
+
+    def MII():
+        return map(int, input().split())
+
+    def LI():
+        return list(input().split())
+
+    def LII():
+        return list(map(int, input().split()))
+
+    def TII():
+        return tuple(map(int, input().split()))
+
+    def GMI():
+        return map(lambda x: int(x) - 1, input().split())
+
+    def LGMI():
+        return list(map(lambda x: int(x) - 1, input().split()))
+
+sys.setrecursionlimit(5 * 10 ** 5)
+# try:
+#     from pypyjit import set_param
+#     set_param('max_unroll_recursion=-1')
+# except ModuleNotFoundError:
+#     pass
+
+dxdy1 = ((0, 1), (0, -1), (1, 0), (-1, 0))  # 上下左右
+dxdy2 = ((0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (-1, -1), (1, -1), (-1, 1))  # 8方向すべて
+dxdy3 = ((0, 1), (1, 0))  # 右 or 下
+dxdy4 = ((1, 1), (1, -1), (-1, 1), (-1, -1))  # 斜め
+
+inf = 1<<60
+
+N,M = LII()
+S = [I() for _ in range(N)]
+
+def create_dist(S):
+    g1=defaultdict(list)
+    g2=defaultdict(list)
+    for i in range(N):
+        s = S[i]
+        for j in range(M):
+            if s[j] == "1":
+                g1[i+j+1].append(i)
+                g2[i].append(i+j+1)
+
+    dist1 = [inf]*N
+    dist1[0] = 0
+    for i in range(1,N):
+        if g1[i]:
+            dist1[i] = min([dist1[j] for j in g1[i]])+1
+
+    dist2 = [inf]*N
+    dist2[N-1] = 0
+    for i in range(0,N-1)[::-1]:
+        if g2[i]:
+            dist2[i] = min([dist2[j] for j in g2[i]])+1
+
+    return dist1,g1,dist2,g2
+
+
+d1,g1,d2,g2 = create_dist(S)
+# print(d1)
+# print(g1)
+# print(d2)
+# print(g2)
+
+ans = []
+for i in range(1,N-1):
+    d = inf
+    for j in range(i+1,i+M):
+        if j >= N:
+            break
+        # print(i,j,g1[j])
+        for p in g1[j]:
+            if p < i:
+                d = min(d, d1[p]+d2[j]+1)
+    if d == inf:
+        d = -1
+    ans.append(d)
+    
+print(*ans)
