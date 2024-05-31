b1235057c3cb535cc3847c52c863ac4fd375c382
masahiro-999 <masahiro999@outlook.com>
2024-05-31 20:26:29 +0900

    abc189_e アファイン変換　回転と平行移動　行列計算

---
+++ abc189/E/main.py
@@ -0,0 +1,150 @@
+from io import BytesIO, IOBase
+
+import sys
+import os
+
+from math import ceil, floor, sqrt, pi, factorial, gcd,lcm,sin,cos,tan,asin,acos,atan2,exp,log,log10
+from bisect import bisect, bisect_left, bisect_right
+from collections import Counter, defaultdict, deque
+from copy import deepcopy
+from functools import cmp_to_key, lru_cache, reduce, cache
+from heapq import heapify, heappop, heappush, heappushpop, nlargest, nsmallest
+from itertools import product, accumulate,permutations,combinations, count
+from operator import add, iand, ior, itemgetter, mul, xor
+from string import ascii_lowercase, ascii_uppercase, ascii_letters
+from typing import *
+from sortedcontainers import SortedSet, SortedList, SortedDict
+
+BUFSIZE = 4096
+
+class FastIO(IOBase):
+    newlines = 0
+
+    def __init__(self, file):
+        self._fd = file.fileno()
+        self.buffer = BytesIO()
+        self.writable = "x" in file.mode or "r" not in file.mode
+        self.write = self.buffer.write if self.writable else None
+
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            if not b:
+                break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
+
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            self.newlines = b.count(b"\n") + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
+
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
+
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
+
+sys.stdin = IOWrapper(sys.stdin)
+sys.stdout = IOWrapper(sys.stdout)
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+
+if True:
+    def I():
+        return input()
+
+    def II():
+        return int(input())
+
+    def MII():
+        return map(int, input().split())
+
+    def LI():
+        return list(input().split())
+
+    def LII():
+        return list(map(int, input().split()))
+
+    def TII():
+        return tuple(map(int, input().split()))
+
+    def GMI():
+        return map(lambda x: int(x) - 1, input().split())
+
+    def LGMI():
+        return list(map(lambda x: int(x) - 1, input().split()))
+
+sys.setrecursionlimit(5 * 10 ** 5)
+# try:
+#     from pypyjit import set_param
+#     set_param('max_unroll_recursion=-1')
+# except ModuleNotFoundError:
+#     pass
+
+dxdy1 = ((0, 1), (0, -1), (1, 0), (-1, 0))  # 上下左右
+dxdy2 = ((0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (-1, -1), (1, -1), (-1, 1))  # 8方向すべて
+dxdy3 = ((0, 1), (1, 0))  # 右 or 下
+dxdy4 = ((1, 1), (1, -1), (-1, 1), (-1, -1))  # 斜め
+
+inf = 1<<60
+
+N = II()
+xy = [LII() for _ in range(N)]
+M = II()
+op = [LII() for _ in range(M)]
+Q = II()
+ab = [LII() for _ in range(Q)]
+
+
+A1 = [[0,1,0],[-1,0,0],[0,0,1]]
+A2 = [[0,-1,0],[1,0,0],[0,0,1]]
+A3=  [[-1,0,None],[0,1,0],[0,0,1]] 
+A4=  [[1,0,0],[0,-1,None],[0,0,1]] 
+
+def mul(A,B):
+    ret = [[0]*3 for _ in range(3)]
+    for i in range(3):
+        for j in range(3):
+            x = 0
+            for k in range(3):
+                x += A[i][k]*B[k][j]
+            ret[i][j] = x
+    return ret
+
+A0 = [[1,0,0],[0,1,0],[0,0,1]]
+
+t = []
+A = [[1,0,0],[0,1,0],[0,0,1]]
+t.append(A)
+
+for i,op in enumerate(op):
+    cmd = op[0]-1
+    p = op[-1]
+
+    A3[0][2] = 2*p
+    A4[1][2] = 2*p
+    A = mul([A1,A2,A3,A4][cmd], A)
+    t.append(A)
+
+# print(t)
+# print(xy)
+for a,b in ab:
+    x,y = xy[b-1]
+    A = t[a]
+    x1 = x*A[0][0]+y*A[0][1]+A[0][2]
+    y1 = x*A[1][0]+y*A[1][1]+A[1][2]
+    print(x1,y1)
