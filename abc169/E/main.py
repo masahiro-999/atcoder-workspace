0fa01c0481e20d4f4edf845277204cb025a6bd24
masahiro-999 <masahiro999@outlook.com>
2024-06-10 21:08:58 +0900

    abc169_e 中央値

---
+++ abc169/E/main.py
@@ -0,0 +1,49 @@
+import sys
+import os
+from math import ceil, floor, sqrt, pi, factorial, gcd,lcm,sin,cos,tan,asin,acos,atan2,exp,log,log10, isqrt
+from collections import Counter, defaultdict, deque
+from copy import deepcopy
+from functools import cmp_to_key, lru_cache, reduce, cache
+from operator import add, iand, ior, itemgetter, mul, xor
+from string import ascii_lowercase, ascii_uppercase, ascii_letters
+from typing import *
+from bisect import bisect, bisect_left, bisect_right
+from heapq import heapify, heappop, heappush, heappushpop, nlargest, nsmallest
+from sortedcontainers import SortedSet, SortedList, SortedDict
+from itertools import product, accumulate,permutations,combinations, count
+input = sys.stdin.readline
+# input = lambda: sys.stdin.readline().rstrip("\r\n")
+I = input
+II = lambda: int(I())
+LI = lambda: list(input().split())
+LII = lambda: list(map(int, input().split()))
+sys.setrecursionlimit(10000000)
+inf = 100100100100
+debug = False
+# debug = True
+if debug:
+    def dprint(*arg): print(*arg, file=sys.stderr)
+else:
+    def dprint(*arg): pass
+
+N = II()
+ab = [LII() for _ in range(N)]
+
+A = [a for a,_ in ab]
+A.sort()
+B = [b for _,b in ab]
+B.sort()
+
+def mid(X):
+    n = len(X)
+    if n%2 ==0:
+        return (X[n//2-1]+X[n//2])
+    else:
+        return X[n//2]
+
+L = mid(A)
+R = mid(B)
+
+ans = R+1-L
+
+print(ans)
