34f6fc2657b42ea75188ae0479f98b46872090b6
masahiro-999 <masahiro999@outlook.com>
2024-06-30 16:32:06 +0900

    arc175_a

---
+++ arc175/A/main.py
@@ -0,0 +1,64 @@
+import sys
+import os
+from math import ceil, floor, sqrt, pi, factorial, gcd,lcm,sin,cos,tan,asin,acos,atan2,exp,log,log10, isqrt
+from collections import Counter, defaultdict, deque
+from copy import deepcopy
+from functools import cmp_to_key, lru_cache, reduce, cache
+from operator import add, iand, ior, itemgetter, mul, xor
+from string import ascii_lowercase, ascii_uppercase, ascii_letters
+from typing import *
+from bisect import bisect, bisect_left, bisect_right
+from heapq import heapify, heappop, heappush, heappushpop, nlargest, nsmallest
+from sortedcontainers import SortedSet, SortedList, SortedDict
+from itertools import product, accumulate,permutations,combinations, count
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+I = input
+II = lambda: int(I())
+LI = lambda: list(input().split())
+LII = lambda: list(map(int, input().split()))
+sys.setrecursionlimit(10000000)
+inf = 100100100100
+debug = False
+# debug = True
+if debug:
+    def dprint(*arg): print(*arg, file=sys.stderr)
+else:
+    def dprint(*arg): pass
+
+MOD = 998244353
+N = II()
+P = LII()
+S = I()
+
+used = set()
+
+# L
+ans1 = 1
+for p in P:
+    p -= 1
+    s = S[p]
+    if s == "R" and  (p+1)%N not in used:
+        ans1 = 0
+        break
+    if s == "?" and (p+1)%N in used:
+        ans1 *= 2
+        ans1 %= MOD
+    used.add((p)%N)
+
+used = set()
+
+# R
+ans2 = 1
+for p in P:
+    p -= 1
+    s = S[p]
+    if s == "L" and  (p)%N not in used:
+        ans2 = 0
+        break
+    if s == "?" and (p)%N in used:
+        ans2 *= 2
+        ans2 %= MOD
+    used.add((p+1)%N)
+# print(ans1)
+# print(ans2)
+print((ans1+ans2)%MOD)
