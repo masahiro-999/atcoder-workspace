ae155d4bf67921a697a949804baf055b28aebc4d
masahiro-999 <masahiro999@outlook.com>
2024-07-06 20:44:24 +0900

    abc186_e extgcd 拡張ユークリッドの互除法による逆元

---
+++ abc186/E/main.py
@@ -0,0 +1,45 @@
+import sys
+import os
+from math import ceil, floor, sqrt, pi, factorial, gcd,lcm,sin,cos,tan,asin,acos,atan2,exp,log,log10, isqrt
+from collections import Counter, defaultdict, deque
+from copy import deepcopy
+from functools import cmp_to_key, lru_cache, reduce, cache
+from operator import add, iand, ior, itemgetter, mul, xor
+from string import ascii_lowercase, ascii_uppercase, ascii_letters
+from typing import *
+from bisect import bisect, bisect_left, bisect_right
+from heapq import heapify, heappop, heappush, heappushpop, nlargest, nsmallest
+from sortedcontainers import SortedSet, SortedList, SortedDict
+from itertools import product, accumulate,permutations,combinations, count
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+I = input
+II = lambda: int(I())
+LI = lambda: list(input().split())
+LII = lambda: list(map(int, input().split()))
+sys.setrecursionlimit(10000000)
+inf = 100100100100
+debug = False
+# debug = True
+if debug:
+    def dprint(*arg): print(*arg, file=sys.stderr)
+else:
+    def dprint(*arg): pass
+T = II()
+
+# Extended Euclidean Algorithm
+def extgcd(a, b):
+    if b:
+        d, y, x = extgcd(b, a % b)
+        y -= (a // b)*x
+        return d, x, y
+    return a, 1, 0
+
+for _ in range(T):
+    N,S,K = LII()
+    gcd_KN,X,Y = extgcd(K,N)
+    if S % gcd_KN == 0:
+        N,S,K = N//gcd_KN, S//gcd_KN, K//gcd_KN
+        ans = -S*X %N
+        print(ans)
+    else:
+        print(-1)
