89c60ac48e6403f4d3f459b47965502418dfc9ea
masahiro-999 <masahiro999@outlook.com>
2024-07-11 08:38:43 +0900

    abc100_D

---
+++ abc100/D/main.py
@@ -0,0 +1,43 @@
+import sys
+import os
+from math import ceil, floor, sqrt, pi, factorial, gcd,lcm,sin,cos,tan,asin,acos,atan2,exp,log,log10, isqrt
+from collections import Counter, defaultdict, deque
+from copy import deepcopy
+from functools import cmp_to_key, lru_cache, reduce, cache
+from operator import add, iand, ior, itemgetter, mul, xor
+from string import ascii_lowercase, ascii_uppercase, ascii_letters
+from typing import *
+from bisect import bisect, bisect_left, bisect_right
+from heapq import heapify, heappop, heappush, heappushpop, nlargest, nsmallest
+from sortedcontainers import SortedSet, SortedList, SortedDict
+from itertools import product, accumulate,permutations,combinations, count
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+I = input
+II = lambda: int(I())
+LI = lambda: list(input().split())
+LII = lambda: list(map(int, input().split()))
+sys.setrecursionlimit(10000000)
+inf = 100100100100
+debug = False
+# debug = True
+if debug:
+    def dprint(*arg): print(*arg, file=sys.stderr)
+else:
+    def dprint(*arg): pass
+
+N,M = LII()
+xyz = [LII() for _ in range(N)]
+
+ans = 0
+for i in range(1<<3):
+    L =[]
+    for d in xyz:
+        sm = 0
+        for j in range(3):
+            sign = 1 if i>>j&1 else -1
+            sm += d[j]*sign
+        L.append(sm)
+    L.sort(reverse=True)
+    ans = max(ans,sum(L[:M]))
+
+print(ans)
