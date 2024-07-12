f52dc20a162fc0f8e7476bc0eaedaa4bbb2daa49
masahiro-999 <masahiro999@outlook.com>
2024-07-12 22:04:34 +0900

    abc098_d 尺取り法

---
+++ abc098/D/main.py
@@ -0,0 +1,50 @@
+import sys
+import os
+from math import ceil, floor, sqrt, pi, factorial, gcd,lcm,sin,cos,tan,asin,acos,atan2,exp,log,log10, isqrt
+from collections import Counter, defaultdict, deque
+from copy import deepcopy
+from functools import cmp_to_key, lru_cache, reduce, cache
+from operator import add, iand, ior, itemgetter, mul, xor
+from string import ascii_lowercase, ascii_uppercase, ascii_letters
+from typing import *
+from bisect import bisect, bisect_left, bisect_right
+from heapq import heapify, heappop, heappush, heappushpop, nlargest, nsmallest
+from sortedcontainers import SortedSet, SortedList, SortedDict
+from itertools import product, accumulate,permutations,combinations, count
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+I = input
+II = lambda: int(I())
+LI = lambda: list(input().split())
+LII = lambda: list(map(int, input().split()))
+sys.setrecursionlimit(10000000)
+inf = 100100100100
+debug = False
+# debug = True
+if debug:
+    def dprint(*arg): print(*arg, file=sys.stderr)
+else:
+    def dprint(*arg): pass
+
+N = II()
+A = LII()
+
+ans = 0
+n = 1
+s = 0
+t = 0
+a = 0
+b = 0
+ans = 0
+for s in range(N):
+    while t<N and a+A[t]==b^A[t]:
+        a += A[t]
+        b ^= A[t]
+        t += 1
+
+    n = t-s
+    ans += n
+    a -= A[s]
+    b ^= A[s]
+
+print(ans)
+
