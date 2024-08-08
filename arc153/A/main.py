fa63544f85df7c4b0dfeb4e0a01adb8bd5f43dd0
masahiro-999 <masahiro999@outlook.com>
2024-08-08 20:23:12 +0900

    arc153_a

---
+++ arc153/A/main.py
@@ -0,0 +1,36 @@
+import sys
+import os
+from math import ceil, floor, sqrt, pi, factorial, gcd,lcm,sin,cos,tan,asin,acos,atan2,exp,log,log10, isqrt
+from collections import Counter, defaultdict, deque
+from copy import deepcopy
+from functools import cmp_to_key, lru_cache, reduce, cache
+from operator import add, iand, ior, itemgetter, mul, xor
+from string import ascii_lowercase, ascii_uppercase, ascii_letters
+from typing import *
+from bisect import bisect, bisect_left, bisect_right
+from heapq import heapify, heappop, heappush, heappushpop, nlargest, nsmallest
+from sortedcontainers import SortedSet, SortedList, SortedDict
+from itertools import product, accumulate,permutations,combinations, count
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+I = input
+II = lambda: int(I())
+LI = lambda: list(input().split())
+LII = lambda: list(map(int, input().split()))
+sys.setrecursionlimit(10000000)
+inf = 100100100100
+debug = False
+# debug = True
+if debug:
+    def dprint(*arg): print(*arg, file=sys.stderr)
+else:
+    def dprint(*arg): pass
+N = II()
+
+S = set()
+for i in range(100000,1000000):
+    s = str(i)
+    x = int(s[0]+s[0]+s[1:3]+s[3]+s[3]+s[4]+s[5]+s[4])
+    S.add(x)
+x = sorted(S)
+ans = x[N-1]
+print(ans)
