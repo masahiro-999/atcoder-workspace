e22e20aa3a287039df3bdccdbb411c5501b7006a
masahiro-999 <masahiro999@outlook.com>
2024-09-04 19:53:26 +0900

    arc142_b

---
+++ arc142/B/main.py
@@ -0,0 +1,44 @@
+import sys
+import os
+from math import ceil, floor, sqrt, pi, factorial, gcd,lcm,sin,cos,tan,asin,acos,atan2,exp,log,log10, isqrt
+from collections import Counter, defaultdict, deque
+from copy import deepcopy
+from functools import cmp_to_key, lru_cache, reduce, cache
+from operator import add, iand, ior, itemgetter, mul, xor
+from string import ascii_lowercase, ascii_uppercase, ascii_letters
+from typing import *
+from bisect import bisect, bisect_left, bisect_right
+from heapq import heapify, heappop, heappush, heappushpop, nlargest, nsmallest
+from sortedcontainers import SortedSet, SortedList, SortedDict
+from itertools import product, accumulate,permutations,combinations, count
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+I = input
+II = lambda: int(I())
+LI = lambda: list(input().split())
+LII = lambda: list(map(int, input().split()))
+sys.setrecursionlimit(10000000)
+inf = 100100100100
+debug = False
+# debug = True
+if debug:
+    def dprint(*arg): print(*arg, file=sys.stderr)
+else:
+    def dprint(*arg): pass
+
+N = II()
+X = [[0]*N for _ in range(N)]
+
+nh1 = (N)//2
+nh2 = N - nh1
+
+for i in range(nh2):
+    for j in range(N):
+        X[i*2][j] = i*N+j+1
+
+for i in range(nh1):
+    for j in range(N):
+        X[i*2+1][j] = (nh2+i)*N+j+1
+
+
+for x in X:
+    print(*x)
