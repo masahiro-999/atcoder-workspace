6f10f4ec25b0ab16622461a5a967e509bfa6f506
masahiro-999 <masahiro999@outlook.com>
2024-09-06 19:59:37 +0900

    arc140_a

---
+++ arc140/A/main.py
@@ -0,0 +1,44 @@
+import sys
+import os
+from math import ceil, floor, sqrt, pi, factorial, gcd,lcm,sin,cos,tan,asin,acos,atan2,exp,log,log10, isqrt
+from collections import Counter, defaultdict, deque
+from copy import deepcopy
+from functools import cmp_to_key, lru_cache, reduce, cache
+from operator import add, iand, ior, itemgetter, mul, xor
+from string import ascii_lowercase, ascii_uppercase, ascii_letters
+from typing import *
+from bisect import bisect, bisect_left, bisect_right
+from heapq import heapify, heappop, heappush, heappushpop, nlargest, nsmallest
+from sortedcontainers import SortedSet, SortedList, SortedDict
+from itertools import product, accumulate,permutations,combinations, count
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+I = input
+II = lambda: int(I())
+LI = lambda: list(input().split())
+LII = lambda: list(map(int, input().split()))
+sys.setrecursionlimit(10000000)
+inf = 100100100100
+debug = False
+# debug = True
+if debug:
+    def dprint(*arg): print(*arg, file=sys.stderr)
+else:
+    def dprint(*arg): pass
+
+N,K = LII()
+S = I()
+
+ans = N
+for i in range(1,N):
+    if N% i == 0:
+        a = 0
+        n1 = N//i
+        d = defaultdict(Counter)
+        for j in range(i):
+            for k in range(n1):
+                d[j][S[j+k*i]] += 1
+        for j in range(i):
+            a+=n1-max(d[j].values())                
+        if a <= K:
+            ans = min(ans,i)
+print(ans)
