228458d7fe1f80d695df0952757f582b6a253947
masahiro-999 <masahiro999@outlook.com>
2024-09-18 20:19:48 +0900

    arc137_b

---
+++ arc137/B/main.py
@@ -0,0 +1,53 @@
+import sys
+import os
+from math import ceil, floor, sqrt, pi, factorial, gcd,lcm,sin,cos,tan,asin,acos,atan2,exp,log,log10, isqrt
+from collections import Counter, defaultdict, deque
+from copy import deepcopy
+from functools import cmp_to_key, lru_cache, reduce, cache
+from operator import add, iand, ior, itemgetter, mul, xor
+from string import ascii_lowercase, ascii_uppercase, ascii_letters
+from typing import *
+from bisect import bisect, bisect_left, bisect_right
+from heapq import heapify, heappop, heappush, heappushpop, nlargest, nsmallest
+from sortedcontainers import SortedSet, SortedList, SortedDict
+from itertools import product, accumulate,permutations,combinations, count
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+I = input
+II = lambda: int(I())
+LI = lambda: list(input().split())
+LII = lambda: list(map(int, input().split()))
+sys.setrecursionlimit(10000000)
+inf = 100100100100
+debug = False
+# debug = True
+if debug:
+    def dprint(*arg): print(*arg, file=sys.stderr)
+else:
+    def dprint(*arg): pass
+
+N = II()
+A = LII()
+
+mi = 0
+p = 0
+sm = 0
+for a in A:
+    if a == 0:
+        sm += 1
+    else:
+        sm -= 1
+    mi = min(mi,sm)
+    p = max(p, sm-mi)
+
+mx = 0
+q = 0
+sm = 0
+for a in A:
+    if a == 0:
+        sm += 1
+    else:
+        sm -= 1
+    mx = max(mx,sm)
+    q = min(q, sm-mx)
+
+print(p-q+1)
