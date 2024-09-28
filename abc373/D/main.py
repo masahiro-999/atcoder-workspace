fce9a3ba8e772ab691b73c1dc1e0bbd6d89c2e6b
masahiro-999 <masahiro999@outlook.com>
2024-09-28 23:00:00 +0900

    abc373_abcd

---
+++ abc373/D/main.py
@@ -0,0 +1,79 @@
+import sys
+import os
+from math import ceil, floor, sqrt, pi, factorial, gcd,lcm,sin,cos,tan,asin,acos,atan2,exp,log,log10, isqrt
+from collections import Counter, defaultdict, deque
+from copy import deepcopy
+from functools import cmp_to_key, lru_cache, reduce, cache
+from operator import add, iand, ior, itemgetter, mul, xor
+from string import ascii_lowercase, ascii_uppercase, ascii_letters
+from typing import *
+from bisect import bisect, bisect_left, bisect_right
+from heapq import heapify, heappop, heappush, heappushpop, nlargest, nsmallest
+from sortedcontainers import SortedSet, SortedList, SortedDict
+from itertools import product, accumulate,permutations,combinations, count
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+I = input
+II = lambda: int(I())
+LI = lambda: list(input().split())
+LII = lambda: list(map(int, input().split()))
+sys.setrecursionlimit(10000000)
+inf = 100100100100100100
+debug = False
+# debug = True
+if debug:
+    def dprint(*arg): print(*arg, file=sys.stderr)
+else:
+    def dprint(*arg): pass
+
+from types import GeneratorType
+
+def bootstrap(f, stack=[]):
+    def wrappedfunc(*args, **kwargs):
+        if stack:
+            return f(*args, **kwargs)
+        else:
+            to = f(*args, **kwargs)
+            while True:
+                if type(to) is GeneratorType:
+                    stack.append(to)
+                    to = next(to)
+                else:
+                    stack.pop()
+                    if not stack:
+                        break
+                    to = stack[-1].send(to)
+            return to
+    return wrappedfunc
+
+N,M = LII()
+
+g = defaultdict(list)
+for i in range(M):
+    u,v,w = LII()
+    u -= 1
+    v -= 1
+    g[u].append((v,w))
+    g[v].append((u,-w))
+
+
+ans = [inf] * (N)
+
+
+@bootstrap
+def dfs(x):
+    for n,w in g[x]:
+        if ans[n] == inf:
+            ans[n] = ans[x] + w
+            yield dfs(n)
+    yield
+
+for i in range(N):
+    if ans[i] == inf:
+        ans[i] = 0
+        dfs(i)
+
+# ans[0] = 0
+# dfs(0)
+
+
+print(*ans)
