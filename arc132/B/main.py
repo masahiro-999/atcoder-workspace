7df782cc20a5c82c6ae24c1a18d9263647bc0b1e
masahiro-999 <masahiro999@outlook.com>
2024-10-03 12:54:09 +0900

    arc132_b

---
+++ arc132/B/main.py
@@ -0,0 +1,45 @@
+import sys
+import os
+from math import ceil, floor, sqrt, pi, factorial, gcd,lcm,sin,cos,tan,asin,acos,atan2,exp,log,log10, isqrt
+from collections import Counter, defaultdict, deque
+from copy import deepcopy
+from functools import cmp_to_key, lru_cache, reduce, cache
+from operator import add, iand, ior, itemgetter, mul, xor
+from string import ascii_lowercase, ascii_uppercase, ascii_letters
+from typing import *
+from bisect import bisect, bisect_left, bisect_right
+from heapq import heapify, heappop, heappush, heappushpop, nlargest, nsmallest
+from sortedcontainers import SortedSet, SortedList, SortedDict
+from itertools import product, accumulate,permutations,combinations, count
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+I = input
+II = lambda: int(I())
+LI = lambda: list(input().split())
+LII = lambda: list(map(int, input().split()))
+sys.setrecursionlimit(10000000)
+inf = 100100100100
+debug = False
+# debug = True
+if debug:
+    def dprint(*arg): print(*arg, file=sys.stderr)
+else:
+    def dprint(*arg): pass
+
+n = II()
+p = LII()
+
+i1 = p.index(1)
+pn  = p.index(n)
+
+if i1 == 0 and pn == n-1:
+    print(0)
+    exit()
+
+if i1 > pn:
+    a1 = i1
+    a2 = 1 + (n-i1)+1
+else:
+    a1 = pn+1
+    a2 = 1 + (n-pn)
+    
+print(min(a1,a2))
