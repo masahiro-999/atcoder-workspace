a44df19e5d86c5c28b14f956bed60cd0bc1605e5
masahiro-999 <masahiro999@outlook.com>
2024-10-09 22:00:00 +0900

    arc129_a

---
+++ arc129/A/main.py
@@ -0,0 +1,39 @@
+import sys
+import os
+from math import ceil, floor, sqrt, pi, factorial, gcd,lcm,sin,cos,tan,asin,acos,atan2,exp,log,log10, isqrt
+from collections import Counter, defaultdict, deque
+from copy import deepcopy
+from functools import cmp_to_key, lru_cache, reduce, cache
+from operator import add, iand, ior, itemgetter, mul, xor
+from string import ascii_lowercase, ascii_uppercase, ascii_letters
+from typing import *
+from bisect import bisect, bisect_left, bisect_right
+from heapq import heapify, heappop, heappush, heappushpop, nlargest, nsmallest
+from sortedcontainers import SortedSet, SortedList, SortedDict
+from itertools import product, accumulate,permutations,combinations, count
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+I = input
+II = lambda: int(I())
+LI = lambda: list(input().split())
+LII = lambda: list(map(int, input().split()))
+sys.setrecursionlimit(10000000)
+inf = 100100100100
+debug = False
+# debug = True
+if debug:
+    def dprint(*arg): print(*arg, file=sys.stderr)
+else:
+    def dprint(*arg): pass
+
+N,L,R = LII()
+
+K = len(f'{N:0b}')
+
+ans = 0
+for k in range(1,K+1):
+    if N >>(k-1)&1:
+        r = min(R,(1<<k)-1)
+        l = max(L,(1<<(k-1)))
+        ans += max(0, r-l+1)
+
+print(ans)
