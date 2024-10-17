2754c144cc28a8d6df2a7d4129417fbe642c4c60
masahiro-999 <masahiro999@outlook.com>
2024-10-17 21:04:44 +0900

    arc127_a

---
+++ arc127/A/main.py
@@ -0,0 +1,49 @@
+import sys
+import os
+from math import ceil, floor, sqrt, pi, factorial, gcd,lcm,sin,cos,tan,asin,acos,atan2,exp,log,log10, isqrt
+from collections import Counter, defaultdict, deque
+from copy import deepcopy
+from functools import cmp_to_key, lru_cache, reduce, cache
+from operator import add, iand, ior, itemgetter, mul, xor
+from string import ascii_lowercase, ascii_uppercase, ascii_letters
+from typing import *
+from bisect import bisect, bisect_left, bisect_right
+from heapq import heapify, heappop, heappush, heappushpop, nlargest, nsmallest
+from sortedcontainers import SortedSet, SortedList, SortedDict
+from itertools import product, accumulate,permutations,combinations, count
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+I = input
+II = lambda: int(I())
+LI = lambda: list(input().split())
+LII = lambda: list(map(int, input().split()))
+sys.setrecursionlimit(10000000)
+inf = 100100100100
+debug = False
+# debug = True
+if debug:
+    def dprint(*arg): print(*arg, file=sys.stderr)
+else:
+    def dprint(*arg): pass
+
+
+N = II()
+K = len(str(N)) 
+
+def count(n):
+    x = int("1"*n)
+    n = 0
+    ret = 0
+    while True:
+        mi = x*(10**n)
+        mx = min((x+1)*(10**n),N+1)
+        if mi > N:
+            break
+        ret += mx - mi
+        n += 1
+    return ret
+
+ans = 0
+for i in range(1, K+1):
+    ans += count(i)
+
+print(ans)
