585bf53a6d65f07a0ce25836b9e59d6e06154b2a
masahiro-999 <masahiro999@outlook.com>
2024-10-22 22:33:25 +0900

    abc187_b

---
+++ abc187/B/main.py
@@ -0,0 +1,43 @@
+import sys
+import os
+from math import ceil, floor, sqrt, pi, factorial, gcd,lcm,sin,cos,tan,asin,acos,atan2,exp,log,log10, isqrt
+from collections import Counter, defaultdict, deque
+from copy import deepcopy
+from functools import cmp_to_key, lru_cache, reduce, cache
+from operator import add, iand, ior, itemgetter, mul, xor
+from string import ascii_lowercase, ascii_uppercase, ascii_letters
+from typing import *
+from bisect import bisect, bisect_left, bisect_right
+from heapq import heapify, heappop, heappush, heappushpop, nlargest, nsmallest
+from sortedcontainers import SortedSet, SortedList, SortedDict
+from itertools import product, accumulate,permutations,combinations, count
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+I = input
+II = lambda: int(I())
+LI = lambda: list(input().split())
+LII = lambda: list(map(int, input().split()))
+sys.setrecursionlimit(10000000)
+inf = 100100100100
+debug = False
+# debug = True
+if debug:
+    def dprint(*arg): print(*arg, file=sys.stderr)
+else:
+    def dprint(*arg): pass
+
+N = II()
+xy = [LII() for _ in range(N)]
+
+ans = 0
+for x1,y1 in xy:
+    for x2,y2 in xy:
+        if x1 == x2 and y1==y2:
+            continue
+        
+        dx = x1 - x2
+        dy = y1 - y2
+
+        if -dx <= dy <= dx:
+            ans += 1
+
+print(ans)
