b34aa64694b5eb021340f227766c9c1ab177dc42
masahiro-999 <masahiro999@outlook.com>
2024-11-10 21:45:31 +0900

    abc379_e

---
+++ abc379/E/main.py
@@ -0,0 +1,48 @@
+import sys
+import os
+from math import ceil, floor, sqrt, pi, factorial, gcd,lcm,sin,cos,tan,asin,acos,atan2,exp,log,log10, isqrt
+from collections import Counter, defaultdict, deque
+from copy import deepcopy
+from functools import cmp_to_key, lru_cache, reduce, cache
+from operator import add, iand, ior, itemgetter, mul, xor
+from string import ascii_lowercase, ascii_uppercase, ascii_letters
+from typing import *
+from bisect import bisect, bisect_left, bisect_right
+from heapq import heapify, heappop, heappush, heappushpop, nlargest, nsmallest
+from sortedcontainers import SortedSet, SortedList, SortedDict
+from itertools import product, accumulate,permutations,combinations, count
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+I = input
+II = lambda: int(I())
+LI = lambda: list(input().split())
+LII = lambda: list(map(int, input().split()))
+sys.setrecursionlimit(10000000)
+inf = 100100100100
+debug = False
+# debug = True
+if debug:
+    def dprint(*arg): print(*arg, file=sys.stderr)
+else:
+    def dprint(*arg): pass
+
+N = II()
+S = I()
+
+A = [0] * N
+for i in range(N):
+    A[i] = int(S[i])*(i+1)
+
+B = list(accumulate(A))
+
+ans = []
+c = 0
+i = 0
+while True:
+    if i < N:
+        c += B[N-i-1]
+    i += 1
+    ans.append(str(c % 10))
+    c //= 10
+    if i >= N and c == 0:
+        break
+print("".join(ans[::-1]))
