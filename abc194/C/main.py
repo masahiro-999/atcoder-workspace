b52f8e229c43af81b75ed203e9c4ca44fcfaaa43
masahiro-999 <masahiro999@outlook.com>
2024-11-11 12:56:19 +0900

    abc194_c

---
+++ abc194/C/main.py
@@ -0,0 +1,41 @@
+import sys
+import os
+from math import ceil, floor, sqrt, pi, factorial, gcd,lcm,sin,cos,tan,asin,acos,atan2,exp,log,log10, isqrt
+from collections import Counter, defaultdict, deque
+from copy import deepcopy
+from functools import cmp_to_key, lru_cache, reduce, cache
+from operator import add, iand, ior, itemgetter, mul, xor
+from string import ascii_lowercase, ascii_uppercase, ascii_letters
+from typing import *
+from bisect import bisect, bisect_left, bisect_right
+from heapq import heapify, heappop, heappush, heappushpop, nlargest, nsmallest
+from sortedcontainers import SortedSet, SortedList, SortedDict
+from itertools import product, accumulate,permutations,combinations, count
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+I = input
+II = lambda: int(I())
+LI = lambda: list(input().split())
+LII = lambda: list(map(int, input().split()))
+sys.setrecursionlimit(10000000)
+inf = 100100100100
+debug = False
+# debug = True
+if debug:
+    def dprint(*arg): print(*arg, file=sys.stderr)
+else:
+    def dprint(*arg): pass
+
+N = II()
+A = LII()
+
+AA = [(i+1)*a*a for i,a in enumerate(A)]
+B = [-2*a*b for a,b in zip(A,accumulate(A))]
+C = [c for c in accumulate([a*a for a in A])]
+
+ans = 0
+for aa,b,c in zip(AA,B,C):
+    ans += aa+b+c
+
+print(ans)
+
+
