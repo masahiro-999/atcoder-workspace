9f452136e2e3994d90bfab59909ef75e313fdc9e
masahiro-999 <masahiro999@outlook.com>
2024-11-13 21:13:02 +0900

    arc128_b

---
+++ arc128/B/main.py
@@ -0,0 +1,45 @@
+import sys
+import os
+from math import ceil, floor, sqrt, pi, factorial, gcd,lcm,sin,cos,tan,asin,acos,atan2,exp,log,log10, isqrt
+from collections import Counter, defaultdict, deque
+from copy import deepcopy
+from functools import cmp_to_key, lru_cache, reduce, cache
+from operator import add, iand, ior, itemgetter, mul, xor
+from string import ascii_lowercase, ascii_uppercase, ascii_letters
+from typing import *
+from bisect import bisect, bisect_left, bisect_right
+from heapq import heapify, heappop, heappush, heappushpop, nlargest, nsmallest
+from sortedcontainers import SortedSet, SortedList, SortedDict
+from itertools import product, accumulate,permutations,combinations, count
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+I = input
+II = lambda: int(I())
+LI = lambda: list(input().split())
+LII = lambda: list(map(int, input().split()))
+sys.setrecursionlimit(10000000)
+inf = 100100100100
+debug = False
+# debug = True
+if debug:
+    def dprint(*arg): print(*arg, file=sys.stderr)
+else:
+    def dprint(*arg): pass
+
+T = II()
+
+def f(R,G,B):
+    if G>R:
+        return inf
+    if R%3 != G%3:
+        return inf
+    else:
+        return R
+
+for _ in range(T):
+    R,G,B = LII()
+    ans = inf
+    for p in permutations([R,G,B]):
+        ans = min(ans, f(*p))
+    if ans == inf:
+        ans = -1
+    print(ans)
