75b89d5ee73de2c3db5c9c750fcde2f3e6de1030
masahiro-999 <masahiro999@outlook.com>
2024-11-30 23:16:25 +0900

    abc382_abcd

---
+++ abc382/C/main.py
@@ -0,0 +1,54 @@
+import sys
+import os
+from math import ceil, floor, sqrt, pi, factorial, gcd,lcm,sin,cos,tan,asin,acos,atan2,exp,log,log10, isqrt
+from collections import Counter, defaultdict, deque
+from copy import deepcopy
+from functools import cmp_to_key, lru_cache, reduce, cache
+from operator import add, iand, ior, itemgetter, mul, xor
+from string import ascii_lowercase, ascii_uppercase, ascii_letters
+from typing import *
+from bisect import bisect, bisect_left, bisect_right
+from heapq import heapify, heappop, heappush, heappushpop, nlargest, nsmallest
+from sortedcontainers import SortedSet, SortedList, SortedDict
+from itertools import product, accumulate,permutations,combinations, count
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+I = input
+II = lambda: int(I())
+LI = lambda: list(input().split())
+LII = lambda: list(map(int, input().split()))
+sys.setrecursionlimit(10000000)
+inf = 100100100100
+debug = False
+# debug = True
+if debug:
+    def dprint(*arg): print(*arg, file=sys.stderr)
+else:
+    def dprint(*arg): pass
+
+N,M = LII()
+A = LII()
+B = LII()
+
+C = []
+mi = inf
+for i,a in enumerate(A):
+    if a >= mi:
+        continue
+    C.append((a,i+1))
+    mi = min(mi, a)
+
+C.sort()
+
+# print(C)
+
+for b in B:
+    i = bisect_left(C,(b,inf))
+    # print(b,i)
+    if i == 0:
+        print(-1)
+    else:
+        x = C[i-1]
+        print(x[1])
+
+
+
