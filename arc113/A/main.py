ec96d4dfc1b046765bf8081c92753e5cd01284e6
masahiro-999 <masahiro999@outlook.com>
2024-12-08 21:03:39 +0900

    arc113_a

---
+++ arc113/A/main.py
@@ -0,0 +1,45 @@
+import sys
+import os
+from math import ceil, floor, sqrt, pi, factorial, gcd,lcm,sin,cos,tan,asin,acos,atan2,exp,log,log10, isqrt
+from collections import Counter, defaultdict, deque
+from copy import deepcopy
+from functools import cmp_to_key, lru_cache, reduce, cache
+from operator import add, iand, ior, itemgetter, mul, xor
+from string import ascii_lowercase, ascii_uppercase, ascii_letters
+from typing import *
+from bisect import bisect, bisect_left, bisect_right
+from heapq import heapify, heappop, heappush, heappushpop, nlargest, nsmallest
+from sortedcontainers import SortedSet, SortedList, SortedDict
+from itertools import product, accumulate,permutations,combinations, count
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+I = input
+II = lambda: int(I())
+LI = lambda: list(input().split())
+LII = lambda: list(map(int, input().split()))
+sys.setrecursionlimit(10000000)
+inf = 100100100100
+debug = False
+# debug = True
+if debug:
+    def dprint(*arg): print(*arg, file=sys.stderr)
+else:
+    def dprint(*arg): pass
+
+K = II()
+
+ans = 0
+
+def f(K):
+    ret = 0
+    for b in range(1,K+1):
+        ret += K//b
+    return(ret)
+
+
+for a in range(1,K+1):
+    bc = K//a
+    x = f(bc)
+    ans += x
+    # print(a,bc,x,ans)
+
+print(ans)
