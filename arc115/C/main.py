c1917ae5111ddfd4a0a94af4df2b2093799c6174
masahiro-999 <masahiro999@outlook.com>
2024-12-17 22:37:19 +0900

    arc115_c

---
+++ arc115/C/main.py
@@ -0,0 +1,65 @@
+import sys
+import os
+from math import ceil, floor, sqrt, pi, factorial, gcd,lcm,sin,cos,tan,asin,acos,atan2,exp,log,log10, isqrt
+from collections import Counter, defaultdict, deque
+from copy import deepcopy
+from functools import cmp_to_key, lru_cache, reduce, cache
+from operator import add, iand, ior, itemgetter, mul, xor
+from string import ascii_lowercase, ascii_uppercase, ascii_letters
+from typing import *
+from bisect import bisect, bisect_left, bisect_right
+from heapq import heapify, heappop, heappush, heappushpop, nlargest, nsmallest
+from sortedcontainers import SortedSet, SortedList, SortedDict
+from itertools import product, accumulate,permutations,combinations, count
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+I = input
+II = lambda: int(I())
+LI = lambda: list(input().split())
+LII = lambda: list(map(int, input().split()))
+sys.setrecursionlimit(10000000)
+inf = 100100100100
+debug = False
+# debug = True
+if debug:
+    def dprint(*arg): print(*arg, file=sys.stderr)
+else:
+    def dprint(*arg): pass
+
+N = II()
+
+def get_prime_list(num_max):
+    prime_table=[1]*(num_max+1)
+    prime_table[0] = 0
+    prime_table[1] = 0
+    for i in range(2, num_max+1):
+        k = i*2
+        while k <= num_max:
+            prime_table[k] = 0
+            k += i
+    return [i for i in range(2,num_max+1) if prime_table[i]]
+
+
+prime_list = get_prime_list(N)
+
+def prime_n(p):
+    ret = {}
+    j = 1
+    while True:
+        pp = p**j
+        k = 1
+        if k*pp >N:
+            break
+        while k*pp <=N:
+            ret[k*pp]=j
+            k += 1
+        j+= 1
+    return ret
+
+ans = [1]*(N+1)
+ans[0]=1
+for p in prime_list:
+    a = prime_n(p)
+    for k,v in a.items():
+        ans[k]+=v
+
+print(*ans[1:])
