0eeef61dff44b3ddcf3ede396be5453885e7db1b
masahiro-999 <masahiro999@outlook.com>
2024-12-29 21:41:35 +0900

    abc386_e

---
+++ abc386/E/main.py
@@ -0,0 +1,75 @@
+import sys
+import os
+from math import ceil, floor, sqrt, pi, factorial, gcd,lcm,sin,cos,tan,asin,acos,atan2,exp,log,log10, isqrt
+from collections import Counter, defaultdict, deque
+from copy import deepcopy
+from functools import cmp_to_key, lru_cache, reduce, cache
+from operator import add, iand, ior, itemgetter, mul, xor
+from string import ascii_lowercase, ascii_uppercase, ascii_letters
+from typing import *
+from bisect import bisect, bisect_left, bisect_right
+from heapq import heapify, heappop, heappush, heappushpop, nlargest, nsmallest
+from sortedcontainers import SortedSet, SortedList, SortedDict
+from itertools import product, accumulate,permutations,combinations, count
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+I = input
+II = lambda: int(I())
+LI = lambda: list(input().split())
+LII = lambda: list(map(int, input().split()))
+sys.setrecursionlimit(10000000)
+inf = 100100100100
+debug = False
+# debug = True
+if debug:
+    def dprint(*arg): print(*arg, file=sys.stderr)
+else:
+    def dprint(*arg): pass
+
+from types import GeneratorType
+
+def bootstrap(f, stack=[]):
+    def wrappedfunc(*args, **kwargs):
+        if stack:
+            return f(*args, **kwargs)
+        else:
+            to = f(*args, **kwargs)
+            while True:
+                if type(to) is GeneratorType:
+                    stack.append(to)
+                    to = next(to)
+                else:
+                    stack.pop()
+                    if not stack:
+                        break
+                    to = stack[-1].send(to)
+            return to
+    return wrappedfunc
+
+N,K = LII()
+A = LII()
+
+t = {}
+
+ans = 0
+
+@bootstrap
+def f(start,K,val):
+    # print(start,K)
+    global ans
+    if K == 0:
+        ans = max(ans, val)
+        yield
+    for i in range(start, N-K+1):
+        yield f(i+1,K-1,val^A[i])
+    yield
+
+if K <= N-K:
+    f(0,K,0)
+    print(ans)
+else:
+    xor = 0
+    for a in A:
+        xor ^= a
+    f(0,N-K,xor)
+    print(ans)
+    
