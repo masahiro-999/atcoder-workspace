1a80daa62f48b5a432bdb67285efcc689bcb149a
masahiro-999 <masahiro999@outlook.com>
2025-01-10 19:52:38 +0900

    arc156_b

---
+++ arc156/B/main.py
@@ -0,0 +1,84 @@
+import sys
+import os
+from math import ceil, floor, sqrt, pi, factorial, gcd,lcm,sin,cos,tan,asin,acos,atan2,exp,log,log10, isqrt, comb
+from collections import Counter, defaultdict, deque
+from copy import deepcopy
+from functools import cmp_to_key, lru_cache, reduce, cache
+from operator import add, iand, ior, itemgetter, mul, xor
+from string import ascii_lowercase, ascii_uppercase, ascii_letters
+from typing import *
+from bisect import bisect, bisect_left, bisect_right
+from heapq import heapify, heappop, heappush, heappushpop, nlargest, nsmallest
+from sortedcontainers import SortedSet, SortedList, SortedDict
+from itertools import product, accumulate,permutations,combinations, count
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+I = input
+II = lambda: int(I())
+LI = lambda: list(input().split())
+LII = lambda: list(map(int, input().split()))
+sys.setrecursionlimit(10000000)
+inf = 100100100100
+debug = False
+# debug = True
+if debug:
+    def dprint(*arg): print(*arg, file=sys.stderr)
+else:
+    def dprint(*arg): pass
+
+MOD = 998244353
+
+def prepare(n, MOD):
+    f = 1
+    factorials = [1]
+    for m in range(1, n + 1):
+        f *= m
+        f %= MOD
+        factorials.append(f)
+    inv = pow(f, MOD - 2, MOD)
+    invs = [1] * (n + 1)
+    invs[n] = inv
+    for m in range(n, 1, -1):
+        inv *= m
+        inv %= MOD
+        invs[m - 1] = inv
+ 
+    return factorials, invs
+
+factorials, invs = prepare(1000000,MOD)
+
+def choose(n,a):
+    if a == 0:
+        return 1
+    if n<a or a <0:
+        return 0
+    return (factorials[n] * (invs[n-a] * invs[a])) % MOD
+
+def H(n,k):
+    return (factorials[n+k-1]*invs[k]*invs[n-1])%MOD
+
+
+N,K = LII()
+A = LII()
+
+setA = set(A)
+
+# 存在しない番号
+notexist = []
+v = 0
+for i in range(K+1):
+    while v in setA:
+        v += 1
+    notexist.append(v)
+    v += 1
+
+# print(notexist)
+
+ans = 0
+for i in range(K+1):
+    max_n = notexist[i]-1
+    if max_n <0:
+        continue
+    k = K-i
+    ans = (ans+H(k+1,max_n))%MOD
+
+print(ans)
