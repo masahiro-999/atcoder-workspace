68158662f452880accbf25553907e7e2a1ed8d76
masahiro-999 <masahiro999@outlook.com>
2025-02-05 21:18:37 +0900

    arc135_c

---
+++ arc135/C/main.py
@@ -0,0 +1,47 @@
+import sys
+import os
+from math import ceil, floor, sqrt, pi, factorial, gcd,lcm,sin,cos,tan,asin,acos,atan2,exp,log,log10, isqrt
+from collections import Counter, defaultdict, deque
+from copy import deepcopy
+from functools import cmp_to_key, lru_cache, reduce, cache
+from operator import add, iand, ior, itemgetter, mul, xor
+from string import ascii_lowercase, ascii_uppercase, ascii_letters
+from typing import *
+from bisect import bisect, bisect_left, bisect_right
+from heapq import heapify, heappop, heappush, heappushpop, nlargest, nsmallest
+from sortedcontainers import SortedSet, SortedList, SortedDict
+from itertools import product, accumulate,permutations,combinations, count
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+I = input
+II = lambda: int(I())
+LI = lambda: list(input().split())
+LII = lambda: list(map(int, input().split()))
+sys.setrecursionlimit(10000000)
+inf = 100100100100
+debug = False
+# debug = True
+if debug:
+    def dprint(*arg): print(*arg, file=sys.stderr)
+else:
+    def dprint(*arg): pass
+
+N = II()
+A = LII()
+
+alive ={i for i in range(N)}
+
+K = 30
+
+one = [sum(A[i]>>k&1 for i in range(N)) for k in range(K)]
+
+ans = 0
+for x in A+[0]:
+    a = 0
+    for i in range(K):
+        n = one[i]
+        if x>>i&1:
+            n = N-n
+        a += (1<<i)*n
+    ans = max(ans,a)
+
+print(ans)
