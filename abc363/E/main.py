67767e1e35a46d1fa33da7ef3afd1c4287182574
masahiro <masa.tsuji@gmail.com>
2025-02-20 08:45:55 +0900

    abc363_e TLE直した　heapqueueで3要素のタプルだとTLEになる謎

--- abc363/E/main.py
+++ abc363/E/main.py
@@ -24,57 +24,31 @@ if debug:
     def dprint(*arg): print(*arg, file=sys.stderr)
 else:
     def dprint(*arg): pass
+
 H,W,Y = LII()
 A = [LII() for _ in range(H)]
 
-# g = defaultdict(list)
+q = []
 
-# for i in range(H):
-#     for j in range(W):
-#         a  = A[i][j]
-#         for dx,dy in ((1,0),(0,1),(-1,0),(0,-1)):
-#             x = i+dx
-#             y = j+dy
-#             if 0<=x<H and 0<=y<W:
-#                 b = A[x][y]
-#                 if a >=b:
-#                     g[(i,j)].append((x,y))
-#                     g[(x,y)].append((i,j))
-
-sl = SortedList()
 visited = [[False]*W for _ in range(H)]
-added = [[False]*W for _ in range(H)]
 
-
-q = [ deque() for _ in range(Y+1)]
-
-n = H*W
 for i in range(H):
-    for j in [0,W-1]:
-        a = A[i][j]
-        if a <= Y:
-            q[a].append((i,j))
+    for j in range(W):
+        if i in (0,H-1) or j in (0,W-1):
+            heappush(q, (A[i][j],(i,j)))
+            visited[i][j] = True
 
-for j in range(1,W-1):
-    for i in [0,H-1]:
-        a = A[i][j]
-        if a <= Y:
-            q[a].append((i,j))
-
-for i in range(1,Y+1):
-    # print(i, sl)
-    while q[i]:
-        x,y = q[i].popleft()
-        if visited[x][y]:
-            continue
-        n -= 1
-        visited[x][y] = True
-        for dx,dy in ((1,0),(0,1),(-1,0),(0,-1)):
-            nx = x+dx
-            ny = y+dy
-            if 0<=nx<H and 0<=ny<W and visited[nx][ny] == False:
-                a = A[nx][ny]
-                if a <= Y:
-                    q[max(i,a)].append((nx,ny))
-
-    print(n)
+cnt = H*W
+for y in range(1,Y+1):
+    # print(y,a,i,j, cnt,used[i][j])
+    while q and q[0][0] <=y:
+        a,(i,j) = heappop(q)
+        cnt -= 1
+        for di,dj in ((-1,0),(1,0),(0,-1),(0,1)):
+            ni = i+di
+            nj = j+dj 
+            if 0<=ni<H and 0<=nj<W and not visited[ni][nj]:
+                visited[ni][nj] = True
+                # print("push", A[ni][nj],ni,nj)
+                heappush(q,(A[ni][nj],(ni,nj)))
+    print(cnt)
