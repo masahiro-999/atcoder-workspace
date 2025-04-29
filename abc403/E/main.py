e5f563826f66c0db4dd402a2aa4008e704940cb3
masahiro-999 <masahiro999@outlook.com>
2025-04-29 20:08:38 +0900

    abc403_e

--- abc403/E/main.py
+++ abc403/E/main.py
@@ -1,62 +1,55 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 import sys
-import os
-from math import ceil, floor, sqrt, pi, factorial, gcd,lcm,sin,cos,tan,asin,acos,atan2,exp,log,log10, isqrt
-from collections import Counter, defaultdict, deque
-from copy import deepcopy
-from functools import cmp_to_key, lru_cache, reduce, cache
-from operator import add, iand, ior, itemgetter, mul, xor
-from string import ascii_lowercase, ascii_uppercase, ascii_letters
-from typing import *
-from bisect import bisect, bisect_left, bisect_right
-from heapq import heapify, heappop, heappush, heappushpop, nlargest, nsmallest
-from sortedcontainers import SortedSet, SortedList, SortedDict
-from itertools import product, accumulate,permutations,combinations, count
+from collections import defaultdict
 input = lambda: sys.stdin.readline().rstrip("\r\n")
 I = input
 II = lambda: int(I())
 LI = lambda: list(input().split())
 LII = lambda: list(map(int, input().split()))
-sys.setrecursionlimit(10000000)
-inf = 100100100100100100100
-debug = False
-# debug = True
-if debug:
-    def dprint(*arg): print(*arg, file=sys.stderr)
-else:
-    def dprint(*arg): pass
 
-p = 1<<61-1
-b = 975993
+@dataclass
+class TrieNode:
+    children: dict = field(default_factory=lambda: defaultdict(TrieNode))
+    is_x_terminal: bool = False
+    count_y: int = 0
 
-def rolling_hash(S):
-    h = 0
-    for c in S:
-        h = (h*b+ord(c))%p
-    return h
+trie_tree = TrieNode()
 
+def add_x(S):
+    node = trie_tree
+    for s in S:
+        node = node.children[s]
+        if node.is_x_terminal:
+            # 既に追加済みの場合は処理不要
+            return 0
+    node.is_x_terminal = True
+    ret = node.count_y
+    add_count_y(S,-ret)
+    return ret
+
+def add_y(S):
+    node = trie_tree
+    for s in S:
+        node = node.children[s]
+        if node.is_x_terminal:
+            # Prefixが一致するXが既にある場合追加しない
+            return 0
+    add_count_y(S, 1)
+    return 1
+
+def add_count_y(S,val):
+    """Trie木の根からSの間の全てのノードにcount_yにvalを足す"""
+    node = trie_tree
+    for s in S:
+        node = node.children[s]
+        node.count_y += val
 
 Q =II()
-query = [LI() for _ in range(Q)]
-
-ans = [0]*Q
-xq = {}
-for q,(t,s) in enumerate(query):
+ans = 0
+for _ in range(Q):
+    t,s = LI()
     if t == "1":
-        x = rolling_hash(s)
-        if x not in xq:
-            xq[x] = q
-
-for q,(t,s) in enumerate(query):
-    if t == "2":
-        ans[q] +=1
-        h = 0
-        a = inf
-        for c in s:
-            h = (h*b+ord(c))%p
-            if h in xq:
-                a = min(a,xq[h])
-        if a != inf:
-            ans[max(q,a)] -= 1
-ans = list(accumulate(ans))
-print(*ans, sep="\n")
+        ans -= add_x(s)
+    else:
+        ans += add_y(s)
+    print(ans)
