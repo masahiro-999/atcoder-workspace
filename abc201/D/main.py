1527685c72082627b6a5aa87a4a656826905f4a4
masahiro <masa.tsuji@gmail.com>
2025-05-28 03:49:27 +0000

    abc201_abcd

--- abc201/D/main.py
+++ abc201/D/main.py
@@ -1,62 +1,56 @@
-import sys, re
-from math import ceil, floor, sqrt, pi, factorial, gcd,sin,cos,tan,asin,acos,atan2,exp,log,log10
-from collections import deque, Counter, defaultdict
-from itertools import product, accumulate
-from functools import reduce,lru_cache
-from bisect import bisect
-from heapq import heapify, heappop, heappush
-sys.setrecursionlimit(5 * 10 ** 5)
-try:
-    from pypyjit import set_param
-    set_param('max_unroll_recursion=-1')
-except ModuleNotFoundError:
-    pass
-input = lambda: sys.stdin.readline().rstrip()
-ii = lambda: int(input())
-mi = lambda: map(int, input().split())
-li = lambda: list(mi())
-inf = 2 ** 63 - 1
-tokens = (i for line in iter(input, "") for i in line.split())
+import sys
+import os
+from math import ceil, floor, sqrt, pi, factorial, gcd,lcm,sin,cos,tan,asin,acos,atan2,exp,log,log10, isqrt
+from collections import Counter, defaultdict, deque
+from copy import deepcopy
+from functools import cmp_to_key, lru_cache, reduce, cache
+from operator import add, iand, ior, itemgetter, mul, xor
+from string import ascii_lowercase, ascii_uppercase, ascii_letters
+from typing import *
+from bisect import bisect, bisect_left, bisect_right
+from heapq import heapify, heappop, heappush, heappushpop, nlargest, nsmallest
+from sortedcontainers import SortedSet, SortedList, SortedDict
+from itertools import product, accumulate,permutations,combinations, count
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+I = input
+II = lambda: int(I())
+LI = lambda: list(input().split())
+LII = lambda: list(map(int, input().split()))
+sys.setrecursionlimit(10000000)
+inf = 100100100100100100100
+debug = False
+# debug = True
+if debug:
+    def dprint(*arg): print(*arg, file=sys.stderr)
+else:
+    def dprint(*arg): pass
 
-    
+H,W = LII()
+A = [I() for _ in range(H)]
 
-def solve(H,W,ab):
-    dp = [[-inf] * W for _ in range(H)]
-    dp[-1][-1] = 0
-    for i in range(H-1,-1,-1):
-        for j in range(W-1,-1,-1):
-            if i == H-1 and j == W-1:
-                continue
-            if j != W-1:
-                dp[i][j] = max(dp[i][j], ab[i][j+1] - dp[i][j+1])
-            if i != H-1:
-                dp[i][j] = max(dp[i][j], ab[i+1][j] - dp[i+1][j])
+dp = [[0]*W for _ in range(H)]
 
-    # @lru_cache(None)
-    # def f(i,j):
-    #     if i == H-1 and j == W-1:
-    #         ret = 0
-    #     else:
-    #         ret = -inf
-    #         if j != W-1:
-    #             ret = max(ret, ab[i][j+1] - f(i,j+1))
-    #         if i != H-1:
-    #             ret = max(ret, ab[i+1][j] - f(i+1,j))
-    #     return ret
+for i in range(H-1,-1,-1):
+    for j in range(W-1,-1,-1):
+        if i == H-1 and j == W-1:
+            ret = 0
+        else:
+            ret = -inf 
+            if i <H-1:
+                x = dp[i+1][j]*-1
+                x += 1 if A[i+1][j] == "+" else -1
+                ret = max(ret,x)
+            if j <W-1:
+                x = dp[i][j+1]*-1
+                x += 1 if A[i][j+1] == "+" else -1
+                ret = max(ret,x)
+        dp[i][j] = ret
 
-    ans = dp[0][0]
-    if ans == 0:
-        print("Draw")
-    elif ans > 0:
-        print("Takahashi")
-    else:
-        print("Aoki")
+ans = dp[0][0]
+if ans >0:
+    print("Takahashi")
+elif ans < 0:
+    print("Aoki")
+else:
+    print("Draw")
 
-def main():
-    H = int(next(tokens))
-    W = int(next(tokens))
-    ab = [[1 if a=="+" else -1 for a in next(tokens)] for _ in range(H)]
-    solve(H,W,ab)
-    return
-
-main()
