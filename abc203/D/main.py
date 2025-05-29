e2a7cc9f553785f6be98a13cea5f3b4e4be1e8b0
masahiro <masa.tsuji@gmail.com>
2025-05-29 23:20:30 +0000

    abc203_abcd

--- abc203/D/main.py
+++ abc203/D/main.py
@@ -1,57 +1,71 @@
-import sys, re
-from math import ceil, floor, sqrt, pi, factorial, gcd,sin,cos,tan,asin,acos,atan2,exp,log,log10
-from collections import deque, Counter, defaultdict
-from itertools import product, accumulate
-from functools import reduce,lru_cache
-from bisect import bisect
-from heapq import heapify, heappop, heappush
-sys.setrecursionlimit(5 * 10 ** 5)
-try:
-    from pypyjit import set_param
-    set_param('max_unroll_recursion=-1')
-except ModuleNotFoundError:
-    pass
-input = lambda: sys.stdin.readline().rstrip()
-ii = lambda: int(input())
-mi = lambda: map(int, input().split())
-li = lambda: list(mi())
-inf = 2 ** 63 - 1
-tokens = (i for line in iter(input, "") for i in line.split())
+import sys
+import os
+from math import ceil, floor, sqrt, pi, factorial, gcd,lcm,sin,cos,tan,asin,acos,atan2,exp,log,log10, isqrt
+from collections import Counter, defaultdict, deque
+from copy import deepcopy
+from functools import cmp_to_key, lru_cache, reduce, cache
+from operator import add, iand, ior, itemgetter, mul, xor
+from string import ascii_lowercase, ascii_uppercase, ascii_letters
+from typing import *
+from bisect import bisect, bisect_left, bisect_right
+from heapq import heapify, heappop, heappush, heappushpop, nlargest, nsmallest
+from sortedcontainers import SortedSet, SortedList, SortedDict
+from itertools import product, accumulate,permutations,combinations, count
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+I = input
+II = lambda: int(I())
+LI = lambda: list(input().split())
+LII = lambda: list(map(int, input().split()))
+sys.setrecursionlimit(10000000)
+inf = 100100100100100100100
+debug = False
+# debug = True
+if debug:
+    def dprint(*arg): print(*arg, file=sys.stderr)
+else:
+    def dprint(*arg): pass
+
+N,K = LII()
+A = [LII() for _ in range(N)]
 
 
-def solve(N: int, K: int, A: "List[List[int]]"):
-    def count_over_n_in_sqr(n,k):
-        # 中央値は、大きい方からk*k//2+1番目の数
-        # 中央値はn以下か？を言い換えると
-        # nを超える数を1にしたとき、k*kの中の1の数がk*k//2+1未満か？
-        a_over_n = [[1 if a > n else 0 for a in a_list] for a_list in A]
-        acc_over_n = [ list(accumulate([0]+a)) for a in a_over_n]
-        col = [[a[j+k] - a[j] for j in range(N-k+1)] for a in acc_over_n] 
-        transpose_col = [list(t) for t in zip(*col)]
-        acc_transpose_col = [list(accumulate([0]+a)) for a in transpose_col]
-        ans  = [[a[j+k] - a[j] for j in range(N-k+1)] for a in acc_transpose_col] 
-        for i in range(N-k+1):
-            for j in range(N-k+1):
-                if ans[i][j] < (k*k//2+1):
-                    return True
-        return False
+def create_t(limit):
+    # limit以下の区画の数
+    t = [[0]*(N+1) for _ in range(N+1)]
+    for i in range(N):
+        for j in range(N):
+            t[i+1][j+1] = t[i+1][j] + (1 if A[i][j]<=limit else 0)
+    for j in range(N):
+        for i in range(N):
+            t[i+1][j+1] += t[i][j+1]    
+    return t
 
-    l = -1 # False 
-    r = 10**9+1 # True
-    while r-l > 1:
-        mid = (r+l)//2
-        if count_over_n_in_sqr(mid,K):
-            r = mid
-        else:
-            l = mid
-    print(r)
-    return
+def f(limit):
+    t = create_t(limit)
+    # print(t)
+    for i in range(N-K+1):
+        for j in range(N-K+1):
+            x = t[i+K][j+K]-t[i][j+K]-t[i+K][j]+t[i][j]
+            # print(i,j,x)
+            if x >= K*K-(K*K//2+1)+1:
+                return True
+    return False
 
-def main():
-    N = int(next(tokens))  # type: int
-    K = int(next(tokens))  # type: int
-    A = [[int(next(tokens)) for _ in range(N)] for _ in range(N)]  # type: "List[List[int]]"
-    solve(N, K, A)
-    return
+# print(f(4))
+"""
+[[0, 0, 0, 0]
+ [0, 1, 1, 2]
+ [0, 2, 2, 3]
+ [0, 2, 3, 5]]
 
-main()
+"""
+ng = -1
+ok = 1000000000
+while ok-ng >1:
+    mid = (ng+ok)//2
+    if f(mid):
+        ok = mid
+    else:
+        ng = mid
+
+print(ok)
