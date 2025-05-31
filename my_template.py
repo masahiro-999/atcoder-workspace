4ba118ba2b9681742bdddccbd4714db782d343c9
masahiro-999 <masahiro999@outlook.com>
2025-05-31 23:17:33 +0900

    template更新

--- my_template.py
+++ my_template.py
@@ -1,6 +1,6 @@
 import sys
 import os
-from math import ceil, floor, sqrt, pi, factorial, gcd,lcm,sin,cos,tan,asin,acos,atan2,exp,log,log10, isqrt
+from math import ceil, floor, sqrt, pi, factorial, gcd,lcm,sin,cos,tan,asin,acos,atan2,exp,log,log10, isqrt, comb
 from collections import Counter, defaultdict, deque
 from copy import deepcopy
 from functools import cmp_to_key, lru_cache, reduce, cache
@@ -10,17 +10,11 @@ from typing import *
 from bisect import bisect, bisect_left, bisect_right
 from heapq import heapify, heappop, heappush, heappushpop, nlargest, nsmallest
 from sortedcontainers import SortedSet, SortedList, SortedDict
-from itertools import product, accumulate,permutations,combinations, count
+from itertools import product, accumulate,permutations,combinations, count, groupby
 input = lambda: sys.stdin.readline().rstrip("\r\n")
 I = input
 II = lambda: int(I())
 LI = lambda: list(input().split())
 LII = lambda: list(map(int, input().split()))
 sys.setrecursionlimit(10000000)
-inf = 100100100100100100100
-debug = False
-# debug = True
-if debug:
-    def dprint(*arg): print(*arg, file=sys.stderr)
-else:
-    def dprint(*arg): pass
+inf = float('inf')
