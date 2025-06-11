4f04c4e86ba689c7d29df2ce3f9cd5a5931b9326
masahiro-999 <masahiro999@outlook.com>
2025-06-11 13:36:45 +0900

    abc216_abcd

--- abc216/D/main.py
+++ abc216/D/main.py
@@ -1,135 +1,56 @@
-from io import BytesIO, IOBase
-
 import sys
 import os
-
-from math import ceil, floor, sqrt, pi, factorial, gcd,lcm,sin,cos,tan,asin,acos,atan2,exp,log,log10
-from bisect import bisect, bisect_left, bisect_right
+from math import ceil, floor, sqrt, pi, factorial, gcd,lcm,sin,cos,tan,asin,acos,atan2,exp,log,log10, isqrt, comb
 from collections import Counter, defaultdict, deque
 from copy import deepcopy
-from functools import cmp_to_key, lru_cache, reduce
-from heapq import heapify, heappop, heappush, heappushpop, nlargest, nsmallest
-from itertools import product, accumulate,permutations,combinations, count
+from functools import cmp_to_key, lru_cache, reduce, cache
 from operator import add, iand, ior, itemgetter, mul, xor
 from string import ascii_lowercase, ascii_uppercase, ascii_letters
 from typing import *
+from bisect import bisect, bisect_left, bisect_right
+from heapq import heapify, heappop, heappush, heappushpop, nlargest, nsmallest
 from sortedcontainers import SortedSet, SortedList, SortedDict
-
-BUFSIZE = 4096
-
-class FastIO(IOBase):
-    newlines = 0
-
-    def __init__(self, file):
-        self._fd = file.fileno()
-        self.buffer = BytesIO()
-        self.writable = "x" in file.mode or "r" not in file.mode
-        self.write = self.buffer.write if self.writable else None
-
-    def read(self):
-        while True:
-            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
-            if not b:
-                break
-            ptr = self.buffer.tell()
-            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
-        self.newlines = 0
-        return self.buffer.read()
-
-    def readline(self):
-        while self.newlines == 0:
-            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
-            self.newlines = b.count(b"\n") + (not b)
-            ptr = self.buffer.tell()
-            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
-        self.newlines -= 1
-        return self.buffer.readline()
-
-    def flush(self):
-        if self.writable:
-            os.write(self._fd, self.buffer.getvalue())
-            self.buffer.truncate(0), self.buffer.seek(0)
-
-class IOWrapper(IOBase):
-    def __init__(self, file):
-        self.buffer = FastIO(file)
-        self.flush = self.buffer.flush
-        self.writable = self.buffer.writable
-        self.write = lambda s: self.buffer.write(s.encode("ascii"))
-        self.read = lambda: self.buffer.read().decode("ascii")
-        self.readline = lambda: self.buffer.readline().decode("ascii")
-
-sys.stdin = IOWrapper(sys.stdin)
-sys.stdout = IOWrapper(sys.stdout)
+from itertools import product, accumulate,permutations,combinations, count, groupby
 input = lambda: sys.stdin.readline().rstrip("\r\n")
+I = input
+II = lambda: int(I())
+LI = lambda: list(input().split())
+LII = lambda: list(map(int, input().split()))
+sys.setrecursionlimit(10000000)
+inf = float('inf')
 
-if True:
-    def I():
-        return input()
+N,M = LII()
 
-    def II():
-        return int(input())
+A = []
+for _ in range(M):
+    k = II()
+    A.append(LII())
 
-    def MII():
-        return map(int, input().split())
+color_pos = [-1]*N
 
-    def LI():
-        return list(input().split())
+pos=[0]*M
 
-    def LII():
-        return list(map(int, input().split()))
+q = []
+for i in range(M):
+    q.append(i)
 
-    def TII():
-        return tuple(map(int, input().split()))
+while q:
+    i = q.pop()
+    c = A[i][pos[i]]-1
+    same_m = color_pos[c]
+    if same_m == -1:
+        color_pos[c]=i
+    else:
+        pos[i]+=1
+        if pos[i]<len(A[i]):
+            q.append(i)
+        pos[same_m]+=1
+        if pos[same_m]<len(A[same_m]):
+            q.append(same_m)
 
-    def GMI():
-        return map(lambda x: int(x) - 1, input().split())
-
-    def LGMI():
-        return list(map(lambda x: int(x) - 1, input().split()))
-
-sys.setrecursionlimit(5 * 10 ** 5)
-try:
-    from pypyjit import set_param
-    set_param('max_unroll_recursion=-1')
-except ModuleNotFoundError:
-    pass
-
-inf = 1<<60
-YES = "Yes"
-NO = "No"
-
-N,M = TII()
-
-def func_read():
-    I()
-    return LII()
-
-A = [func_read() for _ in range(M)]
-
-fifo_list = [deque(a) for a in A]
-next_try = deque(range(M))
-t = defaultdict(lambda : -1)
-empty = 0
-while next_try:
-    i = next_try.popleft()
-    p = fifo_list[i][0]
-    if t[p] == -1:
-        t[p] = i
-        continue
-    if t[p] == i:
-        continue
-    for j in [i,t[p]]:
-        fifo_list[j].popleft()
-        if fifo_list[j]:
-            next_try.append(j)
-        else:
-            empty += 1
-    del t[p]
-
-if empty == M:
-    ans = YES
-else:
-    ans = NO
+ans = "Yes"
+for i in range(M):
+    if pos[i] < len(A[i]):
+        ans = "No"
 
 print(ans)
