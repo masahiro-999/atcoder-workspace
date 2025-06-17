40a3b24bffae16315210734a7ec2bed5411401ad
masahiro-999 <masahiro999@outlook.com>
2025-06-17 21:47:14 +0900

    abc211_abcd

--- abc221/D/main.py
+++ abc221/D/main.py
@@ -1,119 +1,45 @@
-from io import BytesIO, IOBase
-
 import sys
 import os
-
-from math import ceil, floor, sqrt, pi, factorial, gcd,lcm,sin,cos,tan,asin,acos,atan2,exp,log,log10
-from bisect import bisect, bisect_left, bisect_right
+from math import ceil, floor, sqrt, pi, factorial, gcd,lcm,sin,cos,tan,asin,acos,atan2,exp,log,log10, isqrt, comb
 from collections import Counter, defaultdict, deque
 from copy import deepcopy
-from functools import cmp_to_key, lru_cache, reduce
-from heapq import heapify, heappop, heappush, heappushpop, nlargest, nsmallest
-from itertools import product, accumulate,permutations,combinations, count
+from functools import cmp_to_key, lru_cache, reduce, cache
 from operator import add, iand, ior, itemgetter, mul, xor
 from string import ascii_lowercase, ascii_uppercase, ascii_letters
 from typing import *
+from bisect import bisect, bisect_left, bisect_right
+from heapq import heapify, heappop, heappush, heappushpop, nlargest, nsmallest
 from sortedcontainers import SortedSet, SortedList, SortedDict
-
-BUFSIZE = 4096
-
-class FastIO(IOBase):
-    newlines = 0
-
-    def __init__(self, file):
-        self._fd = file.fileno()
-        self.buffer = BytesIO()
-        self.writable = "x" in file.mode or "r" not in file.mode
-        self.write = self.buffer.write if self.writable else None
-
-    def read(self):
-        while True:
-            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
-            if not b:
-                break
-            ptr = self.buffer.tell()
-            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
-        self.newlines = 0
-        return self.buffer.read()
-
-    def readline(self):
-        while self.newlines == 0:
-            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
-            self.newlines = b.count(b"\n") + (not b)
-            ptr = self.buffer.tell()
-            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
-        self.newlines -= 1
-        return self.buffer.readline()
-
-    def flush(self):
-        if self.writable:
-            os.write(self._fd, self.buffer.getvalue())
-            self.buffer.truncate(0), self.buffer.seek(0)
-
-class IOWrapper(IOBase):
-    def __init__(self, file):
-        self.buffer = FastIO(file)
-        self.flush = self.buffer.flush
-        self.writable = self.buffer.writable
-        self.write = lambda s: self.buffer.write(s.encode("ascii"))
-        self.read = lambda: self.buffer.read().decode("ascii")
-        self.readline = lambda: self.buffer.readline().decode("ascii")
-
-sys.stdin = IOWrapper(sys.stdin)
-sys.stdout = IOWrapper(sys.stdout)
+from itertools import product, accumulate,permutations,combinations, count, groupby
 input = lambda: sys.stdin.readline().rstrip("\r\n")
-
-if True:
-    def I():
-        return input()
-
-    def II():
-        return int(input())
-
-    def MII():
-        return map(int, input().split())
-
-    def LI():
-        return list(input().split())
-
-    def LII():
-        return list(map(int, input().split()))
-
-    def TII():
-        return tuple(map(int, input().split()))
-
-    def GMI():
-        return map(lambda x: int(x) - 1, input().split())
-
-    def LGMI():
-        return list(map(lambda x: int(x) - 1, input().split()))
-
-sys.setrecursionlimit(5 * 10 ** 5)
-try:
-    from pypyjit import set_param
-    set_param('max_unroll_recursion=-1')
-except ModuleNotFoundError:
-    pass
-
-inf = 1<<60
+I = input
+II = lambda: int(I())
+LI = lambda: list(input().split())
+LII = lambda: list(map(int, input().split()))
+sys.setrecursionlimit(10000000)
+inf = float('inf')
 
 N = II()
-ab = [TII() for _ in range(N)]
+ab = [LII() for _ in range(N)]
 
-t = defaultdict(int)
-cnt = [0]*N
+A = [a for a,b in ab]
+B = [a+b for a,b in ab]
+
+points = list(sorted(set(A+B)))
+back = {v:i for i,v in enumerate(points)}
+
+table = [0]*len(points)
 
 for a,b in ab:
-    t[a] += 1
-    t[a+b] -= 1
+    point_a = back[a]
+    point_b = back[a+b]
+    table[point_a]+=1
+    table[point_b]-=1
 
-k_list = sorted(t.keys())
 sm = 0
-for i in range(len(k_list)-1):
-    k1 = k_list[i]
-    k2 = k_list[i+1]
-    sm += t[k1]
-    if sm != 0:
-        cnt[sm-1] += (k2-k1)
+ans = [0]*(N+1)
+for i in range(len(points)-1):
+    sm += table[i]
+    ans[sm] += points[i+1]-points[i]
 
-print(*cnt)
+print(*ans[1:])
